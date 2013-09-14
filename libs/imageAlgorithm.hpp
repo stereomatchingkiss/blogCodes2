6369f1ed3b3947cbc6f4c9237994c8ec24515ae5
yyyy <yyyy@yyyys-mac-mini.lan>
2013-09-15 06:58:21 +0800

    add new api

--- libs/imageAlgorithm.hpp
+++ libs/imageAlgorithm.hpp
@@ -41,11 +41,23 @@ cv::Mat calc_back_project(std::initializer_list<cv::Mat> images, std::initialize
  */
 template<size_t N>
 void calc_back_project(std::initializer_list<cv::Mat> images, std::initializer_list<int> channels, cv::InputArray hist, cv::OutputArray backProject,
-                       std::array<float[2], N> ranges, double scale = 1, bool uniform = true)
+                       std::array<float[2], N> const &ranges, double scale = 1, bool uniform = true)
 {
     std::array<float const*, N> d_ranges;
     for(size_t i = 0; i != N; ++i){
         d_ranges[i] = ranges[i];
+    }
+
+    cv::calcBackProject(std::begin(images), images.size(), std::begin(channels), hist, backProject, &d_ranges[0], scale, uniform);
+}
+
+template<size_t N>
+void calc_back_project(std::initializer_list<cv::Mat> images, std::initializer_list<int> channels, cv::InputArray hist, cv::OutputArray backProject,
+                       std::initializer_list<float[2]> ranges, double scale = 1, bool uniform = true)
+{
+    std::array<float const*, N> d_ranges;
+    for(size_t i = 0; i != N; ++i){
+        d_ranges[i] = *(std::begin(ranges) + i);
     }
 
     cv::calcBackProject(std::begin(images), images.size(), std::begin(channels), hist, backProject, &d_ranges[0], scale, uniform);
@@ -59,7 +71,18 @@ void calc_back_project(std::initializer_list<cv::Mat> images, std::initializer_l
 template<size_t N>
 inline
 cv::Mat calc_back_project(std::initializer_list<cv::Mat> images, std::initializer_list<int> channels, cv::InputArray hist,
-                          std::array<float[2], N> ranges, double scale = 1, bool uniform = true)
+                          std::array<float[2], N> const &ranges, double scale = 1, bool uniform = true)
+{
+    cv::Mat output;
+    calc_back_project<N>(images, channels, hist, output, ranges, scale, uniform);
+
+    return output;
+}
+
+template<size_t N>
+inline
+cv::Mat calc_back_project(std::initializer_list<cv::Mat> images, std::initializer_list<int> channels, cv::InputArray hist,
+                          std::initializer_list<float[2]> ranges, double scale = 1, bool uniform = true)
 {
     cv::Mat output;
     calc_back_project<N>(images, channels, hist, output, ranges, scale, uniform);
@@ -113,6 +136,20 @@ void calc_histogram(std::initializer_list<cv::Mat> images, cv::OutputArray outpu
     cv::calcHist(std::begin(images), images.size(), std::begin(channels), mask, output, channels.size(), std::begin(hist_sizes), &d_ranges[0], uniform ,accumulate);
 }
 
+template<size_t N>
+void calc_histogram(std::initializer_list<cv::Mat> images, cv::OutputArray output, std::initializer_list<int> channels,
+                    std::initializer_list<int> hist_sizes, std::initializer_list<float[2]> ranges,
+                    cv::InputArray mask = cv::Mat(), bool uniform = true,
+                    bool accumulate = false)
+{
+    std::array<float const*, N> d_ranges;
+    for(size_t i = 0; i != N; ++i){
+        d_ranges[i] = *(std::begin(ranges) + i);
+    }
+
+    cv::calcHist(std::begin(images), images.size(), std::begin(channels), mask, output, channels.size(), std::begin(hist_sizes), &d_ranges[0], uniform ,accumulate);
+}
+
 /**
  * @brief encapsulation of cv::caclHist, all of the meaning of the parameters are same as the cv::caclHist, but these api
  * are easier to use. ex : cv::Mat hist = calc_histogram({hsv}, {0, 1}, {180, 256}, {{ {0, 180}, {0, 256} }});This api
@@ -124,6 +161,19 @@ template<size_t N>
 inline
 cv::MatND calc_histogram(std::initializer_list<cv::Mat> images, std::initializer_list<int> channels,
                          std::initializer_list<int> hist_sizes, std::array<float[2], N> const &ranges,
+                         cv::InputArray mask = cv::Mat(), bool uniform = true,
+                         bool accumulate = false)
+{
+    cv::MatND output;
+    calc_histogram<N>(images, output, channels, hist_sizes, ranges, mask, uniform, accumulate);
+
+    return output;
+}
+
+template<size_t N>
+inline
+cv::MatND calc_histogram(std::initializer_list<cv::Mat> images, std::initializer_list<int> channels,
+                         std::initializer_list<int> hist_sizes, std::initializer_list<float[2]> ranges,
                          cv::InputArray mask = cv::Mat(), bool uniform = true,
                          bool accumulate = false)
 {
