10f5e12c96d9788a7ef010eb1efdf06831f22922
yyyy <yyyy@yyyys-mac-mini.lan>
2013-10-22 13:12:33 +0800

    refine comments

--- spiritExercises/exercise_00/exercise00.hpp
+++ spiritExercises/exercise_00/exercise00.hpp
@@ -157,7 +157,10 @@ void exercise_00()
     result.clear();
     std::back_insert_iterator<std::string> sink(number);
     size_t const Size = video.size() - 1;
-    //use karma to generate it is a little bit overkill, it is only for practice
+    //use karma to generate the time label is a little bit overkill
+    //since the length of the time label are known, sprintf or stringstream
+    //should be able to do the job(even it may slower than spirit).
+    //However, for practice, I chose spirit::karma to generate the time label
     client::videoGrammar<std::back_insert_iterator<std::string>> grammar;
     client::transformData data{0, std::vector<std::vector<int>>(2, std::vector<int>(4))};
     for(size_t i = 0; i != Size; ++i){
