6272b1d0c05075a007b6de3cf9e1ab807ba624e7
yyyy <yyyy@yyyys-mac-mini.lan>
2013-10-24 16:41:15 +0800

    add new example

--- spiritExercises/exercise_00/main.cpp
+++ spiritExercises/exercise_00/main.cpp
@@ -1,17 +1,22 @@
+#include <cstring>
+#include <cstdio>
+
 #include <iostream>
+
+#include <timeEstimate.hpp>
 
 #include "exercise00.hpp"
 #include "simpleDelimiter.hpp"
+#include "stackOverflowQ00.hpp"
 
 #include "lexWordCount.hpp"
 
 int main()
 {    
     //test_exercise_00();
-    test_simple_delimiter();
+    //test_simple_delimiter();
     //test_lex_word_count_1();
-
+    test_so_00();
 
     return 0;
 }
-
