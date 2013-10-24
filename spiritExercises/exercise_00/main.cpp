9d8c2f48d27b532e7304e0835a3962039da2565f
yyyy <yyyy@yyyys-mac-mini.lan>
2013-10-25 02:27:20 +0800

    add new test

--- spiritExercises/exercise_00/main.cpp
+++ spiritExercises/exercise_00/main.cpp
@@ -1,11 +1,13 @@
 #include <cstring>
 #include <cstdio>
 
+#include <algorithm>
 #include <iostream>
 
 #include <timeEstimate.hpp>
 
 #include "exercise00.hpp"
+#include "recursiveBraces.hpp"
 #include "simpleDelimiter.hpp"
 #include "stackOverflowQ00.hpp"
 
@@ -16,7 +18,8 @@ int main()
     //test_exercise_00();
     //test_simple_delimiter();
     //test_lex_word_count_1();
-    test_so_00();
+    //test_so_00();
+    test_parse_recursive_braces();
 
     return 0;
 }
