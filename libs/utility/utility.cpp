c48cde6115c0075d48fe701e9636be9f34dcd1ef
yyyy <yyyy@yyyys-mac-mini.lan>
2013-10-31 02:26:16 +0800

    add new functions

--- libs/utility/utility.cpp
+++ libs/utility/utility.cpp
@@ -1,9 +1,20 @@
-#include <cmath>
-
 #include "utility.hpp"
 
 namespace OCV
 {
+
+void draw_rects(cv::Mat &inout, std::vector<cv::Rect> const &rects)
+{
+    cv::Point2f points[4];
+    cv::Scalar const color = cv::Scalar(255, 0, 0);
+    for(auto const &rect : rects){
+        get_rect_points(rect, points);
+        cv::line(inout, {points[0].x, points[0].y}, {points[1].x, points[1].y}, color, 2);
+        cv::line(inout, {points[1].x, points[1].y}, {points[2].x, points[2].y}, color, 2);
+        cv::line(inout, {points[2].x, points[2].y}, {points[3].x, points[3].y}, color, 2);
+        cv::line(inout, {points[3].x, points[3].y}, {points[0].x, points[0].y}, color, 2);
+    }
+}
 
 /**
  * @brief remove the contours by contours area(pixels surrounded by the contours)
--- libs/utility/utility.hpp
+++ libs/utility/utility.hpp
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <initializer_list>
+#include <numeric>
 #include <vector>
 
 #include <opencv2/core/core.hpp>
@@ -10,6 +11,27 @@
 
 namespace OCV
 {
+
+void draw_rects(cv::Mat &inout, std::vector<cv::Rect> const &rects);
+
+/**
+ * @brief generate points from the rectangle by clockwise order(top left,
+ * top right, bottom right, bottom left)
+ * @param rect   : input retangle
+ * @param points : generated points
+ */
+template<typename Point>
+void get_rect_points(cv::Rect const &rect, Point points[])
+{
+    points[0].x = rect.x;
+    points[0].y = rect.y;
+    points[1].x = rect.x + rect.width - 1;
+    points[1].y = rect.y;
+    points[2].x = points[1].x;
+    points[2].y = rect.y + rect.height - 1;
+    points[3].x = rect.x;
+    points[3].y = points[2].y;
+}
 
 inline void mix_channels(cv::Mat const &src, cv::Mat &dst, std::initializer_list<int> from_to)
 {
