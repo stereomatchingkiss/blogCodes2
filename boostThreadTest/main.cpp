4757421406e7d582b4edf6139ec1b507b7edbd61
stereomatchingkiss <stereomatchingkiss@gmail.com>
2014-06-09 21:21:49 +0800

    first commit

--- boostThreadTest/main.cpp
+++ boostThreadTest/main.cpp
@@ -1,4 +1,5 @@
 #include "asioAndSignal2.hpp"
+#include "asioSignal2Test.hpp"
 #include "waitConditionTest.hpp"
 #include "waitFutureTest.hpp"
 
@@ -6,7 +7,8 @@
 
 int main()
 {
-    asio_and_signal2();
+    //asio_and_signal2();
+    asio_and_signal2_test();
     //wait_condition_00();
     //wait_future_test_00();
 
