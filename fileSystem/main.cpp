490407a13c60a60d03763aad1058d720b6afb272
stereomatchingkiss <stereomatchingkiss@gmail.com>
2014-06-14 02:06:22 +0800

    refine codes

--- fileSystem/main.cpp
+++ fileSystem/main.cpp
@@ -11,15 +11,14 @@ int main(int argc, char *argv[])
 {    
     if(argc == 1){
         move_libs();
-    }else{
-        std::string const Path(argv[1]);
+    }else{        
         std::set<std::string> filter;
         if(argc > 2){
             for(int i = 2; i != argc; ++i){
                 filter.emplace(argv[i]);
             }
         }
-        move_libs(Path, filter);
+        move_libs(argv[1], filter);
     }
 
     return 0;
