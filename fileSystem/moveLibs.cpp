fc874bfb9e4f5ceecf5d41eeb24fdab65acd9bae
stereomatchingkiss <stereomatchingkiss@gmail.com>
2014-06-14 01:15:41 +0800

    refine codes

--- fileSystem/moveLibs.cpp
+++ fileSystem/moveLibs.cpp
@@ -9,13 +9,13 @@ void move_libs(std::string const &target, std::set<std::string> const &filter)
     using RecurSiveDirIter = bf::recursive_directory_iterator;
 
     boost::filesystem::create_directory(target);
+    auto const &CurrentPath = bf::current_path().generic_string();
+    auto const TargetDir = CurrentPath + "/" + target + "/";
 
-    std::string const &CurrentPath = bf::current_path().generic_string();
     for(RecurSiveDirIter dir(CurrentPath), end; dir != end; ++dir){
         if(boost::filesystem::is_regular_file(*dir)){
             bf::path info(*dir);
-            if(filter.find(info.extension().generic_string()) != std::end(filter)){
-                auto const TargetDir = CurrentPath + "/" + target + "/";
+            if(filter.find(info.extension().generic_string()) != std::end(filter)){                
                 boost::filesystem::rename(*dir,
                                           TargetDir + info.filename().generic_string());
             }
