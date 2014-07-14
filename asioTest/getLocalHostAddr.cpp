ee8dbdca790154a265326a8aa9d51577d8ec1888
stereomatchingkiss <stereomathingkiss@gmai.com>
2014-07-14 13:51:48 +0800

    refine test case

--- asioTest/getLocalHostAddr.cpp
+++ asioTest/getLocalHostAddr.cpp
@@ -12,12 +12,17 @@ void get_local_host_address()
     boost::asio::io_service io;
     boost::asio::ip::tcp::resolver resolver(io);
     boost::asio::ip::tcp::resolver::query query(boost::asio::ip::host_name(), "");
+    std::cout<<boost::asio::ip::host_name()<<std::endl;
     boost::asio::ip::tcp::resolver::iterator iter = resolver.resolve(query);
     boost::asio::ip::tcp::resolver::iterator end;
     while(iter != end)
     {
-        if(iter->endpoint().address().is_v4()){
+        if(iter->endpoint().address().is_v4()){            
             std::cout << iter->endpoint().address().to_string() << std::endl;
+            std::cout << iter->endpoint().address().to_v4().to_ulong()<<std::endl;
+            std::cout << static_cast<int>(iter->endpoint().address().to_v4().to_ulong())
+                      << std::endl;
+            //std::cout << iter->endpoint().address().to_v4() << std::endl;
         }
         ++iter;
     }
--- asioTest/main.cpp
+++ asioTest/main.cpp
@@ -12,7 +12,8 @@
 int main()
 {
     std::vector<std::string> const Names{"dispatch example 00", "no work 00",
-                                         "post example 00", "timer example 00"};
+                                         "post example 00", "timer example 00",
+                                         "getLocalHostAddr"};
 
     for(size_t i = 0; i != Names.size(); ++i){
         std::cout<<i<<" : "<<Names[i]<<std::endl;
@@ -21,16 +22,19 @@ int main()
     size_t input = 0;
     std::cin>>input;
     if(input < Names.size()){
-        std::vector<std::function<void()>> func
-        {
-            dispatch_example_00, no_work_00,
-                    post_example_00, timer_00
-        };
+        std::vector<std::function<void()>> func{
+                                           dispatch_example_00, no_work_00,
+                                           post_example_00, timer_00,
+                                           get_local_host_address
+    };
 
         func[input]();
     }else{
         std::cout<<"out of range"<<std::endl;
     }
+    std::cout<<"program exist"<<std::endl;
+    std::cin.get();
+    std::cin.get();
 
     return 0;
 }
