2fde8ef5b6aa29fc5528cd5cb077d34a37cd9d27
stereomatchingkiss <stereomatchingkiss@gmail.com>
2014-08-04 16:23:16 +0800

    first commit

---
+++ boostPhoenix/find_if.hpp
@@ -0,0 +1,4 @@
+#ifndef FIND_IF_HPP
+#define FIND_IF_HPP
+
+#endif // FIND_IF_HPP
---
+++ libs/testFunction/testHelper.hpp
@@ -0,0 +1,25 @@
+#ifndef TESTHELPER_HPP
+#define TESTHELPER_HPP
+
+#include <functional>
+#include <iostream>
+#include <vector>
+
+template<typename T>
+void register_test_case(T const &names, std::vector<std::function<void()>> &funcs)
+{
+    for(size_t i = 0; i != names.size(); ++i){
+        std::cout<<i<<" : "<<names[i]<<std::endl;
+    }
+
+    size_t input = 0;
+    std::cin>>input;
+    if(input < names.size()){
+        funcs[input]();
+    }else{
+        std::cout<<"out of range"<<std::endl;
+    }
+    std::cout<<"program exist"<<std::endl;
+}
+
+#endif // TESTHELPER_HPP
