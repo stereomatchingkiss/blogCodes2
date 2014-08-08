c96cdd4ac17099b9429427bc636eb2673ae42a8c
stereomatchingkiss <stereomathingkiss@gmai.com>
2014-08-08 15:24:57 +0800

    const correctness

--- libs/testFunction/testHelper.hpp
+++ libs/testFunction/testHelper.hpp
@@ -6,7 +6,7 @@
 #include <vector>
 
 template<typename T>
-void register_test_case(T const &names, std::vector<std::function<void()>> &funcs)
+void register_test_case(T const &names, std::vector<std::function<void()>> const &funcs)
 {
     for(size_t i = 0; i != names.size(); ++i){
         std::cout<<i<<" : "<<names[i]<<std::endl;
