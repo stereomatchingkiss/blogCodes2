18420bc5d510f13c44ae2be7b975ec1c8f51bb14
stereomatchingkiss <stereomatchingkiss@gmail.com>
2014-09-27 19:53:03 +0800

    remove useless codes

--- smallApps/processtestResult.hpp
+++ smallApps/processtestResult.hpp
@@ -17,30 +17,13 @@ class processPutGetTestResult
         Vec<> elapse_;
         Vec<> frequency_;
         Vec<std::string> put_types_;
-    };
-
-    struct logStructure
-    {
-        std::string dashs_;
-        std::string revision_;//by now this contains lot of message, temp solution
-        std::string change_path_flag_;
-        std::vector<std::string> commit_files_;
-        std::string commit_month_;
-        size_t commit_day_;
-        size_t commit_year_;
-        std::string user_name_;
-        std::vector<std::string> commit_messages_;
-    };
+    };    
 
 public:
     processPutGetTestResult();
 
     void average_time();
-    void compare_two_performance_test_file();
-    void generate_change_log(std::string const &log_name);
-
-    void proces_change_log(logStructure const &log, std::ofstream &out);
-    logStructure read_change_log(std::ifstream &in);
+    void compare_two_performance_test_file();    
 
 private:
     fileData get_data(std::string const &file_name);
