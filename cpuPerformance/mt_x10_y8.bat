ab6711d2681d9657273b87e02bd9896a62f0dced
stereomatchingkiss <stereomathingkiss@gmai.com>
2014-10-23 12:36:31 +0800

    update execution params

--- cpuPerformance/mt_x10_y8.bat
+++ cpuPerformance/mt_x10_y8.bat
@@ -2,7 +2,7 @@ set var=mt_x10_y8
 
 rem openNotification.bat
 
-start "" "G:\Jobs\codes\sysdev\scada\src\unitTest\rtdbTestSuite\build-performanceTest-vc2008_Qt4_8_5-Release\release\performanceTest.exe" "mt" "mt_x10_y8"
+start "" "G:\Jobs\codes\sysdev\scada\src\unitTest\rtdbTestSuite\build-performanceTest-vc2008_Qt4_8_5-Release\release\performanceTest.exe" "mt_get_put" "mt_x10_y8"
 
 start "" "python" cpuUsageMonitor.pyw rtdb "%var%"
 start "" "python" cpuUsageMonitor.pyw performanceTest "%var%"
