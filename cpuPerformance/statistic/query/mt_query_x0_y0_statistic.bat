c7ca41716a9badbb94dcdc87ec937d583d8c1697
stereomatchingkiss <stereomathingkiss@gmai.com>
2014-10-24 12:37:29 +0800

    first commit

---
+++ cpuPerformance/statistic/query/mt_query_x0_y0_statistic.bat
@@ -0,0 +1,4 @@
+smallApps.exe rtdb_mt_query_x0_y0.txt
+smallApps.exe performanceTest_mt_query_x0_y0.txt
+smallApps.exe cpuUsage_mt_query_x0_y0.txt 1
+pause
---
+++ cpuPerformance/statistic/query/query_x0_y0_statistic.bat
@@ -0,0 +1,4 @@
+smallApps.exe rtdb_query_x0_y0.txt
+smallApps.exe performanceTest_query_x0_y0.txt
+smallApps.exe cpuUsage_query_x0_y0.txt 1
+pause
