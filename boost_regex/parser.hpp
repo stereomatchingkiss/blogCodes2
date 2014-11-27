035d35112ff7fb6086c5b0dd41c9862e755b6ec3
stereomatchingkiss <stereomathingkiss@gmai.com>
2014-11-27 15:51:45 +0800

    add new function to parse words

--- boost_regex/parser.hpp
+++ boost_regex/parser.hpp
@@ -25,7 +25,35 @@ public:
     std::string get_match_word(std::string const &pattern,
                                StrVec const &input) const;
 
+    template<typename OutputIter>
+    std::string get_match_word(std::string const &pattern,
+                               OutputIter begin,
+                               OutputIter end) const;
+
 private:
 };
+
+/**
+ *@brief match the word(only the first word will be match) of pattern\n
+ * specify
+ *
+ *@param pattern regular expression(perl style)
+ *@param begin begin position
+ *@param end past the end position
+ */
+template<typename OutputIter>
+std::string parser::get_match_word(const std::string &pattern,
+                                   OutputIter begin,
+                                   OutputIter end) const
+{
+    for(OutputIter it = begin; it != end; ++it){
+        std::string result = parser_->get_match_word(pattern, *it);
+        if(!result.empty()){
+            return result;
+        }
+    }
+
+    return "";
+}
 
 #endif // FILE_PARSER_HPP
