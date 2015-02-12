bf9281a8dddd1502305cd682ba0570284cb6789a
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-02-12 22:28:56 +0800

    test modify example

--- boost_multiindex/main.cpp
+++ boost_multiindex/main.cpp
@@ -1,10 +1,12 @@
 #include "employee.hpp"
 #include "employee_sequence.hpp"
+#include "modify_example.hpp"
 
 int main()
 {    
     //employee_example();
-    employee_sequence();
+    //employee_sequence();
+    order_after_modify();
 
     return 0;
 }
