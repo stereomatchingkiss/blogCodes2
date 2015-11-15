d0817fb0b1461f72e413cb8d06411e2925f0d4ab
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-11-16 06:59:57 +0800

    first commit

---
+++ ANPR2/main.cpp
@@ -0,0 +1,29 @@
+#include "license_plate_localizer.hpp"
+
+#include <ocv_libs/cmd/command_prompt_utility.hpp>
+#include <ocv_libs/file/utility.hpp>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{               
+    //auto const image_name = "baza_slika/040603/P6040009.jpg";
+    //auto const image_name = "baza_slika/280503/P5280115.jpg";
+    std::string const Path = "baza_slika/train";
+    auto images =
+            ocv::file::get_directory_files(Path);
+    images.resize(1);
+    //images[0] = "2014-Audi-R8-V10-Plus.jpg";    
+    for(auto const &im_name : images){
+        std::cout<<(Path + "/" + im_name)<<std::endl;
+        cv::Mat input = cv::imread(Path + "/" + im_name);
+        if(!input.empty()){            
+            license_plate_localizer lpl;
+            std::vector<cv::Rect> plate_regions;
+            lpl.localize_plate(input, plate_regions);
+        }
+    }//*/
+}
