abdf384f845a62cb172477efbc55430bc419aff8
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-11-18 16:21:45 +0800

    move preprocessor to make file

--- ANPR2/fhog_trainer.cpp
+++ ANPR2/fhog_trainer.cpp
@@ -1,6 +1,5 @@
 #include "fhog_trainer.hpp"
 
-#define DLIB_JPEG_SUPPORT
 #include <dlib/svm_threaded.h>
 #include <dlib/gui_widgets.h>
 #include <dlib/data_io.h>
@@ -17,8 +16,8 @@ void fhog_number_plate_trainer::preprocess(std::string const &train_xml,
     load_image_dataset(images_test, face_boxes_test,
                        test_xml);
 
-    upsample_image_dataset<pyramid_down<2> >(images_train, face_boxes_train);
-    upsample_image_dataset<pyramid_down<2> >(images_test,  face_boxes_test);
+    //upsample_image_dataset<pyramid_down<2> >(images_train, face_boxes_train);
+    //upsample_image_dataset<pyramid_down<2> >(images_test,  face_boxes_test);
 
     //add_image_left_right_flips(images_train, face_boxes_train);
 }
@@ -39,35 +38,39 @@ fhog_number_plate_trainer::fhog_number_plate_trainer(int argc, char **argv)
             using image_scanner_type = scan_fhog_pyramid<pyramid_down<10>>;
 
             image_scanner_type scanner;
-            scanner.set_detection_window_size(80, 20);
+            scanner.set_detection_window_size(120, 40);
             //scanner.set_nuclear_norm_regularization_strength(1.0);
             structural_object_detection_trainer<image_scanner_type> trainer(scanner);
             trainer.set_num_threads(std::thread::hardware_concurrency());
-            trainer.set_c(15);
+            trainer.set_c(12);
+            trainer.set_match_eps(0.4);
             //trainer.be_verbose();
-            trainer.set_epsilon(0.01);
+            trainer.set_epsilon(0.005);
 
             object_detector<image_scanner_type> detector =
                     trainer.train(images_train, face_boxes_train);
 
-            std::cout << "training results(precision,recall,AP): " << test_object_detection_function(detector, images_train, face_boxes_train) << std::endl;
-            std::cout << "testing results(precision,recall,AP):  " << test_object_detection_function(detector, images_test, face_boxes_test) << std::endl;
+            std::cout << "training results(precision,recall,AP): "
+                      << test_object_detection_function(detector, images_train, face_boxes_train);
 
-            //image_window win;
+            //upsample_image_dataset<pyramid_down<2> >(images_test, face_boxes_test);
+            std::cout << "testing results(precision,recall,AP):  "
+                      << test_object_detection_function(detector, images_test, face_boxes_test);
+
+            image_window win;
             for(size_t i = 0; i < images_test.size(); ++i)
             {
-                // Run the detector and get the face detections.
+                // Run the detector and get the number plate detections
                 std::vector<rectangle> dets = detector(images_test[i]);
                 std::cout<<i<<" detect "<<dets.size()<<" number plate"<<std::endl;
-                /*win.clear_overlay();
-                win.set_image(images_train[i]);
+                win.clear_overlay();
+                win.set_image(images_test[i]);
                 win.add_overlay(dets, rgb_pixel(255,0,0));
                 std::cout << "Hit enter to process the next image..." << std::endl;
-                std::cin.get();*/
-                for(auto const Rect : dets){
-                    std::cout<<Rect<<std::endl;
-                }
+                std::cin.get();
             }
+
+            serialize("number_plate_detector.svm") << detector;
         }
 
     }catch(std::exception const &ex){
