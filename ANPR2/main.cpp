ffc2a44bac3155f2ab82f4f340798affcb786b2d
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-11-18 08:04:51 +0800

    test train results of fhog

--- ANPR2/main.cpp
+++ ANPR2/main.cpp
@@ -1,3 +1,4 @@
+#include "fhog_trainer.hpp"
 #include "morphology_localizer.hpp"
 
 #include <ocv_libs/cmd/command_prompt_utility.hpp>
@@ -8,22 +9,36 @@
 
 #include <iostream>
 
+template<typename Localizer = morphology_localizer>
+void test_number_plate_localizer(int argc, char **argv);
+
 int main(int argc, char **argv)
 {               
-    //auto const image_name = "baza_slika/040603/P6040009.jpg";
-    //auto const image_name = "baza_slika/280503/P5280115.jpg";
-    std::string const Path = "baza_slika/test";
-    auto images = ocv::file::get_directory_files(Path);
-    morphology_localizer lpl;
-    lpl.set_show_debug_message(true);
-    //images.resize(1);
-    //images[0] = "2014-Audi-R8-V10-Plus.jpg";    
-    for(auto const &im_name : images){
-        std::cout<<(Path + "/" + im_name)<<std::endl;
-        cv::Mat input = cv::imread(Path + "/" + im_name);
-        if(!input.empty()){                        
-            std::vector<cv::Rect> plate_regions;
-            lpl.localize_plate(input, plate_regions);
+    fhog_number_plate_trainer fhog_trainer(argc, argv);
+    //test_number_plate_localizer<>(argc, argv);
+
+}
+
+template<typename Localizer>
+void test_number_plate_localizer(int argc, char **argv)
+{
+    auto const map =
+            ocv::cmd::default_command_line_parser(argc, argv).first;
+    if(map.count("image_folder")){
+        auto const Path = map["image_folder"].as<std::string>();
+        auto images = ocv::file::get_directory_files(Path);
+        Localizer lpl;
+        lpl.set_show_debug_message(true);
+        //images.resize(10);
+        for(auto const &im_name : images){
+            std::cout<<(Path + "/" + im_name)<<std::endl;
+            cv::Mat input = cv::imread(Path + "/" + im_name);
+            if(!input.empty()){
+                std::vector<cv::Rect> plate_regions;
+                lpl.localize_plate(input, plate_regions);
+            }
         }
-    }//*/
+    }else{
+        std::cerr<<"must input folder path"<<std::endl;
+    }
 }
