a48bfbe8ec5b579dcf6ef7c42c9717c31135e76e
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-03 13:00:14 +0800

    only prune the characters if the contours size larger than max char num

--- ANPR2/prune_illegal_chars.cpp
+++ ANPR2/prune_illegal_chars.cpp
@@ -8,30 +8,30 @@ prune(cv::Mat const &plate,
       size_t max_char_num,
       contours_type &contours)
 {
-    dims_.resize(0);
-    for(int i = 0; i != contours.size(); ++i){
-        auto const rect =cv::boundingRect(contours[i]);
-        dims_.push_back(rect.height + rect.y);
-    }
-
-    diff_.clear();
-    for(int r = 0; r != dims_.rows; ++r){
-        cv::absdiff(dims_, dims_.at<int>(r,0), dims_);
-        diff_.emplace_back(cv::sum(dims_)[0], std::move(contours[r]));
-    }
-    std::sort(std::begin(diff_), std::end(diff_),
-              [](auto const &lhs, auto const &rhs)
-    {
-        return lhs.first< rhs.first;
-    });
-    for(size_t i = 0; i != diff_.size(); ++i){
-        contours[i] = std::move(diff_[i].second);
-    }
     if(contours.size() > max_char_num){
-        contours.resize(max_char_num);
-    }
+        dims_.resize(0);
+        for(int i = 0; i != contours.size(); ++i){
+            auto const rect =cv::boundingRect(contours[i]);
+            dims_.push_back(rect.height + rect.y);
+        }
 
-    show_prune_results(plate, contours);
+        diff_.clear();
+        for(int r = 0; r != dims_.rows; ++r){
+            cv::absdiff(dims_, dims_.at<int>(r,0), dims_);
+            diff_.emplace_back(cv::sum(dims_)[0], std::move(contours[r]));
+        }
+        std::sort(std::begin(diff_), std::end(diff_),
+                  [](auto const &lhs, auto const &rhs)
+        {
+            return lhs.first< rhs.first;
+        });
+        for(size_t i = 0; i != diff_.size(); ++i){
+            contours[i] = std::move(diff_[i].second);
+        }
+        contours.resize(max_char_num);
+
+        show_prune_results(plate, contours);
+    }
 }
 
 void prune_illegal_chars::set_show_debug_message(bool value)
