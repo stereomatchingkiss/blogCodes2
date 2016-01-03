81bff00c031e7cf0a0f856bc5ca286e138cebe31
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-03 12:20:21 +0800

    first commit

---
+++ ANPR2/prune_illegal_chars.cpp
@@ -0,0 +1,54 @@
+#include "prune_illegal_chars.hpp"
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+void prune_illegal_chars::
+prune(cv::Mat const &plate,
+      size_t max_char_num,
+      contours_type &contours)
+{
+    dims_.resize(0);
+    for(int i = 0; i != contours.size(); ++i){
+        auto const rect =cv::boundingRect(contours[i]);
+        dims_.push_back(rect.height + rect.y);
+    }
+
+    diff_.clear();
+    for(int r = 0; r != dims_.rows; ++r){
+        cv::absdiff(dims_, dims_.at<int>(r,0), dims_);
+        diff_.emplace_back(cv::sum(dims_)[0], std::move(contours[r]));
+    }
+    std::sort(std::begin(diff_), std::end(diff_),
+              [](auto const &lhs, auto const &rhs)
+    {
+        return lhs.first< rhs.first;
+    });
+    for(size_t i = 0; i != diff_.size(); ++i){
+        contours[i] = std::move(diff_[i].second);
+    }
+    if(contours.size() > max_char_num){
+        contours.resize(max_char_num);
+    }
+
+    show_prune_results(plate, contours);
+}
+
+void prune_illegal_chars::set_show_debug_message(bool value)
+{
+    debug_ = value;
+}
+
+void prune_illegal_chars::show_prune_results(cv::Mat const &plate,
+                                             contours_type const &contours) const
+{
+    if(debug_){
+        cv::Mat zero = cv::Mat::zeros(plate.size(),CV_8U);
+        for(int i = 0; i != contours.size(); ++i){
+            cv::drawContours(zero, contours, i, {255}, -1);
+        }
+        cv::imshow("prune_results", zero);
+        cv::waitKey();
+        cv::destroyWindow("prune_results");
+    }
+}
---
+++ ANPR2/prune_illegal_chars.hpp
@@ -0,0 +1,32 @@
+#ifndef PRUNE_ILLEGAL_CHARS_HPP
+#define PRUNE_ILLEGAL_CHARS_HPP
+
+#include <opencv2/core.hpp>
+
+#include <vector>
+
+class prune_illegal_chars
+{
+public:
+    using contour_type = std::vector<cv::Point>;
+    using contours_type = std::vector<contour_type>;
+
+    prune_illegal_chars() = default;
+
+    void prune(cv::Mat const &plate,
+               size_t max_char_num,
+               contours_type &contours);
+
+    void set_show_debug_message(bool value);
+
+private:
+    void show_prune_results(cv::Mat const &plate,
+                            contours_type const &contours) const;
+
+    bool debug_ = false;
+
+    std::vector<std::pair<int,contour_type>> diff_;
+    cv::Mat_<int> dims_;
+};
+
+#endif
