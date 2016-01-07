f78d8f093ddc2af5e86de9b38b28ade0dbac61cd
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-07 14:38:02 +0800

    simplify codes

--- ANPR2/grab_character.cpp
+++ ANPR2/grab_character.cpp
@@ -25,6 +25,30 @@ grab_character::grab_character() :
     char_count_.insert({"*", 0});
 }
 
+void grab_character::save_char(cv::Mat const &plate,
+                               cv::Rect const &rect,
+                               int key)
+{
+    std::string const map_key(1, char(key));
+    std::string folder;
+    std::string file_symbol;
+    if(map_key != "*"){
+        folder = chars_folder_ + "/" + map_key;
+        file_symbol = map_key;
+    }else{
+        folder = chars_folder_ + "/negative";
+        file_symbol = "negative";
+    }
+    if(!boost::filesystem::exists(folder)){
+        boost::filesystem::create_directory(folder);
+    }
+    std::ostringstream ostream;
+    ostream<<folder<<"/"<<file_symbol<<"_"<<std::setfill('0')
+          <<std::setw(4)<<char_count_[map_key]++;
+    cv::imwrite(ostream.str() + ".png",
+                plate(rect));
+}
+
 void grab_character::
 grab_chars(cv::Mat const &plate,
            contours_type const &contours)
@@ -50,24 +74,7 @@ grab_chars(cv::Mat const &plate,
             }else if(key == 'q'){
                 break;
             }else{
-                std::string const map_key(1, char(key));
-                std::string folder;
-                std::string file_symbol;
-                if(map_key != "*"){
-                    folder = chars_folder_ + "/" + map_key;
-                    file_symbol = map_key;
-                }else{
-                    folder = chars_folder_ + "/negative";
-                    file_symbol = "negative";
-                }
-                if(!boost::filesystem::exists(folder)){
-                    boost::filesystem::create_directory(folder);
-                }
-                std::ostringstream ostream;
-                ostream<<folder<<"/"<<file_symbol<<"_"<<std::setfill('0')
-                      <<std::setw(4)<<char_count_[map_key]++;
-                cv::imwrite(ostream.str() + ".png",
-                            plate(rect));
+                save_char(plate, rect, key);
                 cv::destroyAllWindows();
             }
         }catch(std::exception const &ex){
