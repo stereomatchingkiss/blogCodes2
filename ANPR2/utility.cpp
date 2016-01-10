01cc54102726c4aa065e9a3782cf48cc6ef88c8b
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-10 10:25:07 +0800

    first commit

---
+++ ANPR2/utility.cpp
@@ -0,0 +1,22 @@
+#include "utility.hpp"
+
+#include <opencv2/imgproc.hpp>
+
+#include <vector>
+
+void binarize_image(cv::Mat const &input, cv::Mat &output)
+{
+    cv::Mat hsv;
+    cv::cvtColor(input, hsv, CV_BGR2HSV);
+    std::vector<cv::Mat> hsv_split;
+    cv::split(hsv, hsv_split);
+    cv::Mat intensity;
+    cv::GaussianBlur(hsv_split[2], intensity, {7,7}, 0);
+
+    constexpr int blockSize = 11;
+    constexpr double offset = 9;
+    cv::adaptiveThreshold(intensity, output, 255,
+                          cv::ADAPTIVE_THRESH_MEAN_C,
+                          cv::THRESH_BINARY_INV,
+                          blockSize, offset);
+}
---
+++ ANPR2/utility.hpp
@@ -0,0 +1,9 @@
+#ifndef UTILITY_HPP
+#define UTILITY_HPP
+
+#include <opencv2/core.hpp>
+
+void binarize_image(cv::Mat const &input,
+                    cv::Mat &output);
+
+#endif
