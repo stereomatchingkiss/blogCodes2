d1b2f62c84431dafa9b4d58aa8a4013cee213f4b
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-13 21:09:31 +0800

    add folder recognition mode

--- face_recognition/main.cpp
+++ face_recognition/main.cpp
@@ -2,6 +2,9 @@
 #include "face_detector.hpp"
 #include "face_recognition.hpp"
 
+#include <ocv_libs/core/resize.hpp>
+#include <ocv_libs/file/utility.hpp>
+
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -16,6 +19,15 @@ using vmap = boost::program_options::variables_map;
 int capture_face(vmap const &command);
 int images_record(vmap const &command);
 int recognize_face(vmap const &command);
+int recognize_from_camera(std::string const &train_folder,
+                          size_t train_size);
+int recognize_from_folder(std::string const &train_folder,
+                          std::string const &input_folder,
+                          size_t train_size);
+
+void draw_detect_face(cv::Mat &inout,
+                      face_recognition &fr,
+                      std::vector<cv::Rect> const &regions);
 
 vmap parse_command_line(int argc, char **argv);
 
@@ -49,6 +61,26 @@ int capture_face(vmap const &command)
     return capture.capture();
 }
 
+void draw_detect_face(cv::Mat &inout,
+                      face_recognition &fr,
+                      std::vector<cv::Rect> const &regions)
+{
+    for(auto const &rect : regions){
+        auto const name = fr.recognize(inout(rect));
+        cv::Point const point = {std::max(0, rect.x - rect.x/5),
+                                 std::max(0, rect.y - 30)};
+        if(!name.empty()){
+            cv::rectangle(inout, rect, {0,255,0}, 2);
+            cv::putText(inout, name, point,
+                        cv::FONT_HERSHEY_COMPLEX, 1.0, {0,255,0}, 2);
+        }else{
+            cv::putText(inout, "unknown", point,
+                        cv::FONT_HERSHEY_COMPLEX, 1.0, {0,0,255}, 2);
+            cv::rectangle(inout, rect, {0,0,255}, 2);
+        }
+    }
+}
+
 vmap parse_command_line(int argc, char **argv)
 {
     using namespace boost::program_options;
@@ -62,10 +94,12 @@ vmap parse_command_line(int argc, char **argv)
                                                               "1 will enter recogniton mode;"
                                                               "2 will enter video record mode"
                                                               "default value is 1")
-                ("input_folder,i", value<std::string>(), "Specify the input folders")
+                ("input_folder,i", value<std::string>(), "Specify the input folder")
                 ("record_folder,v", value<std::string>(), "Specify the folder of record files")
                 ("output_folder,o", value<std::string>(), "Specify the output folder")
-                ("random_size,r", value<size_t>()->default_value(0), "Specify the random size of faces");
+                ("train_size,r", value<size_t>()->default_value(0), "Specify the training size of "
+                                                                    "each face category")
+                ("train_folder,t", value<std::string>(), "Specify the train folder");
 
         variables_map vm;
         store(parse_command_line(argc, argv, desc), vm);
@@ -131,44 +165,74 @@ int images_record(vmap const &command)
 
 
 int recognize_face(vmap const &command)
-{
-    if(!command.count("input_folder")){
-        std::cout<<"must specify input_folder"<<std::endl;
+{    
+    if(!command.count("train_folder")){
+        std::cout<<"must specify train_folder"<<std::endl;
         return -1;
     }
 
-    auto const input_folder = command["input_folder"].as<std::string>();
-    auto const random_size = command["random_size"].as<size_t>();
+    if(!command.count("input_folder")){
+        auto const train_folder = command["train_folder"].as<std::string>();
+        auto const train_size = command["train_size"].as<size_t>();
 
+        return recognize_from_camera(train_folder, train_size);
+    }else{
+        auto const train_folder = command["train_folder"].as<std::string>();
+        auto const input_folder = command["input_folder"].as<std::string>();
+        auto const train_size = command["train_size"].as<size_t>();
+
+        return recognize_from_folder(train_folder,
+                                     input_folder, train_size);
+    }
+
+    return 0;
+}
+
+int recognize_from_camera(std::string const &train_folder,
+                          size_t train_size)
+{
     cv::VideoCapture cap(0);
     if(!cap.isOpened()){
         std::cout<<"cannot open camera"<<std::endl;
         return -1;
     }
 
-    face_recognition fr(input_folder, random_size);
+    face_recognition fr(train_folder, train_size);
     face_detector fd;
     cv::Mat frame;
     while(true){
         cap.read(frame);
         if(!frame.empty()){
             auto const &regions = fd.detect(frame);
-            for(size_t i = 0; i != regions.size(); ++i){
-                auto const name = fr.recognize(frame(regions[i]));
-                cv::Point const point = {std::max(0, regions[i].x - regions[i].x/5),
-                                         std::max(0, regions[i].y - 30)};
-                if(!name.empty()){
-                    cv::rectangle(frame, regions[i], {0,255,0}, 2);
-                    cv::putText(frame, name, point,
-                                cv::FONT_HERSHEY_COMPLEX, 1.0, {0,255,0}, 2);
-                }else{
-                    cv::putText(frame, "unknown", point,
-                                cv::FONT_HERSHEY_COMPLEX, 1.0, {0,0,255}, 2);
-                    cv::rectangle(frame, regions[i], {0,0,255}, 2);
-                }
-            }
+            draw_detect_face(frame, fr, regions);
             cv::imshow("recognize mode", frame);
             int const key = cv::waitKey(30);
+            if(key == 'q'){
+                break;
+            }
+        }else{
+            break;
+        }
+    }
+
+    return 0;
+}
+
+int recognize_from_folder(std::string const &train_folder,
+                          std::string const &input_folder,
+                          size_t train_size)
+{
+    auto const files = ocv::file::get_directory_files(input_folder);
+    face_recognition fr(train_folder, train_size);
+    face_detector fd;
+    for(size_t i = 0; i != files.size(); ++i){
+        auto img = cv::imread(input_folder + "/" + files[i]);
+        if(!img.empty()){
+            ocv::resize_aspect_ratio(img, img, {640,0});
+            auto const &regions = fd.detect(img);
+            draw_detect_face(img, fr, regions);
+            cv::imshow("recognize mode", img);
+            int const key = cv::waitKey();
             if(key == 'q'){
                 break;
             }
