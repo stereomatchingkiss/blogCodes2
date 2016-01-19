e7843da0d14e35695fa77d1731b7671e96539dc3
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-19 15:07:46 +0800

    1 : Only recognize 6 characters plate with specific pattern 2 : Make recognize api become more flexible

--- ANPR2/bbps_char_recognizer.cpp
+++ ANPR2/bbps_char_recognizer.cpp
@@ -5,19 +5,40 @@
 #include <opencv2/ml.hpp>
 
 bbps_char_recognizer::
-bbps_char_recognizer(cv::Ptr<cv::ml::StatModel> rec,
+bbps_char_recognizer(cv::Ptr<cv::ml::StatModel> alpha_rec,
+                     cv::Ptr<cv::ml::StatModel> num_rec,
                      ocv::block_binary_pixel_sum<> const &bbps) :
-    bbps_(bbps),    
-    rec_(rec)
+    alpha_rec_(alpha_rec),
+    bbps_(bbps),
+    num_rec_(num_rec)
 {
     generate_map(bimap_);
 }
 
 std::string bbps_char_recognizer::
-recognize(const cv::Mat&, const cv::Mat &binary_input)
+recognize(cv::Mat const&, cv::Mat const &binary_input,
+          regions_type const &regions)
+{
+    std::string result;
+    if(regions.size() >= 6){
+        for(size_t i = 0; i != 2; ++i){
+            result += recognize(binary_input(regions[i]), alpha_rec_);
+        }
+        for(size_t i = 2; i != 5; ++i){
+            result += recognize(binary_input(regions[i]), num_rec_);
+        }
+        result += recognize(binary_input(regions[5]), alpha_rec_);
+    }
+
+    return result;
+}
+
+std::string bbps_char_recognizer::
+recognize(cv::Mat const& character,
+          cv::Ptr<cv::ml::StatModel> const &rec)
 {
     int const label =
-            static_cast<int>(rec_->predict(bbps_.describe(binary_input)));
+            static_cast<int>(rec->predict(bbps_.describe(character)));
     auto it = bimap_.right.find(label);
     if(it != std::end(bimap_.right)){
         return it->second;
--- ANPR2/bbps_char_recognizer.hpp
+++ ANPR2/bbps_char_recognizer.hpp
@@ -19,33 +19,47 @@ class StatModel;
 
 }
 
+/**
+ * Recognize plate with 6 characters, the pattern is
+ * w{2}d{3}w{1}
+ */
 class bbps_char_recognizer
 {
-public:
+public:    
+    using regions_type = std::vector<cv::Rect>;
+
     /**
      * Construct object with the recognizer, the recognizer
      * expect the features extract by the class
      * block_binary_pixel_sum<float>
-     * @param rec The recognizer of the character
+     * @param alpha_rec The recognizer of alphabet
+     * @param num_rec The recognizer of number
      * @param bbps The feature extractor of the character recognizer
      */
-    explicit bbps_char_recognizer(cv::Ptr<cv::ml::StatModel> rec,
-                                  ocv::block_binary_pixel_sum<> const &bbps = ocv::block_binary_pixel_sum<>());
+    explicit bbps_char_recognizer(cv::Ptr<cv::ml::StatModel> alpha_rec,
+                                  cv::Ptr<cv::ml::StatModel> num_rec,
+                                  ocv::block_binary_pixel_sum<> const &bbps = ocv::block_binary_pixel_sum<>());    
 
     /**
      * Recognize the input character
      * @param input Character before process
      * @param binary_input Character after processed,
      * should be a binary image with values, 0 or 255
+     * @param regions regions of characters
      * @return The character of input should be
      */
     std::string recognize(cv::Mat const &input,
-                          cv::Mat const &binary_input);
+                          cv::Mat const &binary_input,
+                          regions_type const &regions);
 
 private:    
+    std::string recognize(cv::Mat const &character,
+                          cv::Ptr<cv::ml::StatModel> const &rec);
+
+    cv::Ptr<cv::ml::StatModel> alpha_rec_;
     ocv::block_binary_pixel_sum<> bbps_;
     boost::bimap<std::string, int> bimap_;
-    cv::Ptr<cv::ml::StatModel> rec_;
+    cv::Ptr<cv::ml::StatModel> num_rec_;
 };
 
 #endif
