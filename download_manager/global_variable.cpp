3423749ef10addb2fb76efc077aa49dc33c063bb
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-23 09:51:15 +0800

    add pause variable

--- download_manager/global_variable.cpp
+++ download_manager/global_variable.cpp
@@ -7,6 +7,7 @@ namespace global{
 QString const done = "Done";
 QString const downloading = "Downloading";
 QString const error = "Error : ";
+QString const pause = "Pause";
 QString const starting = "Starting";
 QString const waiting = "Waiting";
 QString const unknown = "?";
--- download_manager/global_variable.hpp
+++ download_manager/global_variable.hpp
@@ -10,6 +10,7 @@ namespace global{
 extern QString const done;
 extern QString const downloading;
 extern QString const error;
+extern QString const pause;
 extern QString const starting;
 extern QString const waiting;
 extern QString const unknown;
