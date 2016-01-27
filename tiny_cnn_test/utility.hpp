daf09669d23e0fb9dfb77ca300db48895a87afeb
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-28 01:50:37 +0800

    fix bugs--type error;should not push back element by parallel api

---
+++ tiny_cnn_test/utility.hpp
@@ -0,0 +1,55 @@
+#ifndef UTILITY_HPP
+#define UTILITY_HPP
+
+#include <ocv_libs/core/for_each.hpp>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <string>
+#include <vector>
+
+template<typename Img>
+Img cvmat_to_img(cv::Mat const& input,
+                 float_t scale_min = -1,
+                 float_t scale_max = 1,
+                 int x_padding = 0,
+                 int y_padding = 0)
+{
+    CV_Assert(input.type() == CV_8U);
+
+    if (x_padding < 0 || y_padding < 0){
+        throw std::runtime_error("padding size must not be negative");
+    }
+    if (scale_min >= scale_max){
+        throw std::runtime_error("scale_max must be greater than scale_min");
+    }
+
+    Img img;
+    if (x_padding || y_padding){
+        int const w = input.cols + 2 * x_padding;
+        int const h = input.rows + 2 * y_padding;
+        int const area = w * h;
+        float const range = scale_max - scale_min;
+        img.resize(area * input.channels(), scale_min);
+
+        for (int c = 0; c < input.channels(); ++c) {
+            for (int y = 0; y < input.rows; ++y) {
+                for (int x = 0; x < input.cols; ++x) {
+                    img[c * area + (y + y_padding) * w + x + x_padding]
+                            = scale_min + range *
+                            input.at<cv::Vec3b>(y, x)[c] / 255.0;
+                }
+            }
+        }
+    }else{
+        ocv::for_each<uchar>(input, [&](uchar c)
+        {
+           img.emplace_back(scale_min + (scale_max - scale_min) * c / 255);
+        });
+    }
+
+    return img;
+}
+
+#endif // UTILITY_HPP
