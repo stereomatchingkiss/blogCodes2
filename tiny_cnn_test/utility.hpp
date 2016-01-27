c1fc4144b9f2c3996f9ce45b545f15f4cf369142
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-28 07:38:42 +0800

    remove padding function

--- tiny_cnn_test/utility.hpp
+++ tiny_cnn_test/utility.hpp
@@ -12,31 +12,26 @@
 template<typename Img>
 Img cvmat_to_img(cv::Mat const& input,
                  float_t scale_min = -1,
-                 float_t scale_max = 1,
-                 int x_padding = 0,
-                 int y_padding = 0)
+                 float_t scale_max = 1)
 {
-    CV_Assert(input.type() == CV_8U);
+    CV_Assert(input.type() == CV_8U || input.type() == CV_8UC3);
 
-    if (x_padding < 0 || y_padding < 0){
-        throw std::runtime_error("padding size must not be negative");
-    }
     if (scale_min >= scale_max){
         throw std::runtime_error("scale_max must be greater than scale_min");
     }
 
     Img img;
-    if (x_padding || y_padding){
-        int const w = input.cols + 2 * x_padding;
-        int const h = input.rows + 2 * y_padding;
+    if (input.type() == CV_8UC3){
+        int const w = input.cols;
+        int const h = input.rows;
         int const area = w * h;
         float const range = scale_max - scale_min;
-        img.resize(area * input.channels(), scale_min);
+        img.resize(area * input.channels(), scale_min);        
 
         for (int c = 0; c < input.channels(); ++c) {
             for (int y = 0; y < input.rows; ++y) {
                 for (int x = 0; x < input.cols; ++x) {
-                    img[c * area + (y + y_padding) * w + x + x_padding]
+                    img[c * area + y * w + x]
                             = scale_min + range *
                             input.at<cv::Vec3b>(y, x)[c] / 255.0;
                 }
@@ -45,7 +40,7 @@ Img cvmat_to_img(cv::Mat const& input,
     }else{
         ocv::for_each<uchar>(input, [&](uchar c)
         {
-           img.emplace_back(scale_min + (scale_max - scale_min) * c / 255);
+           img.emplace_back(scale_min + (scale_max - scale_min) * c / 255.0);
         });
     }
 
