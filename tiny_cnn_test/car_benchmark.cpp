409135843498eeff24749ee3bab80d0d73842be4
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-29 12:28:37 +0800

    able to train a network to classify car or non car

--- tiny_cnn_test/car_benchmark.cpp
+++ tiny_cnn_test/car_benchmark.cpp
@@ -2,7 +2,6 @@
 #include "utility.hpp"
 
 #include <ocv_libs/file/utility.hpp>
-#include <ocv_libs/ml/utility/split_train_test.hpp>
 #include <ocv_libs/tiny_cnn/trainer.hpp>
 
 #include <boost/progress.hpp>
@@ -19,57 +18,86 @@ using namespace tiny_cnn::activation;
 
 namespace{
 
+cv::ImreadModes const imread_modes = cv::IMREAD_GRAYSCALE;
+
 template<typename Net>
 void create_lenet(Net &nn)
 {
     using activate = relu;
-    nn << convolutional_layer<activate>(32, 32, 5, 1, 6) // C1, 1@32x32-in, 6@28x28-out
-       << average_pooling_layer<activate>(28, 28, 6, 2) // S2, 6@28x28-in, 6@14x14-out
-       << convolutional_layer<activate>(14, 14, 5, 6, 16) // C3, 6@14x14-in, 16@10x10-in
-       << average_pooling_layer<activate>(10, 10, 16, 2) // S4, 16@10x10-in, 16@5x5-out
-       << convolutional_layer<activate>(5, 5, 5, 16, 120) // C5, 16@5x5-in, 120@1x1-out
-       << fully_connected_layer<activate>(120, 2); // F6, 120-in, 10-out
+    /*nn << convolutional_layer<activate>(32, 32, 5, 1, 6)
+       << average_pooling_layer<activate>(28, 28, 6, 2)
+       << convolutional_layer<activate>(14, 14, 5, 6, 16)
+       << average_pooling_layer<activate>(10, 10, 16, 2)
+       << convolutional_layer<activate>(5, 5, 5, 16, 120)
+       << fully_connected_layer<softmax>(120, 2);//*/
+
+    nn << convolutional_layer<activate>(32, 32, 3, 1, 6, padding::same)
+       << max_pooling_layer<activate>(32, 32, 6, 2)
+       << convolutional_layer<activate>(16, 16, 3, 6, 16, padding::same)
+       << max_pooling_layer<activate>(16, 16, 16, 2)
+       << convolutional_layer<activate>(8, 8, 5, 16, 24)
+       << fully_connected_layer<softmax>(384, 10);//*/
+
+    /*nn << convolutional_layer<activate>(32, 32, 3, 1, 6, padding::same)
+       << max_pooling_layer<activate>(32, 32, 6, 2)
+       << convolutional_layer<activate>(16, 16, 3, 6, 16, padding::same)
+       << max_pooling_layer<activate>(16, 16, 16, 2)
+       << convolutional_layer<activate>(8, 8, 3, 16, 24, padding::same)
+       << max_pooling_layer<activate>(8, 8, 24, 2)
+       << convolutional_layer<activate>(4, 4, 3, 24, 32, padding::same)
+       << fully_connected_layer<softmax>(512, 2);//*/
 }
 
 template<typename Net>
 void create_minivgg(Net &nn)
 {
     using activate = relu;
-    dropout_layer drop_00(16*16*6, 0.25);
-    dropout_layer drop_01(8*8*16, 0.25);
-    dropout_layer drop_02(4*4*24, 0.5);
-    nn << convolutional_layer<activate>(32, 32, 3, 1, 6, padding::same)
-       << convolutional_layer<activate>(32, 32, 3, 6, 6, padding::same)
-       << max_pooling_layer<activate>(32, 32, 6, 2)
-          //<< drop_00
-       << convolutional_layer<activate>(16, 16, 3, 6, 16, padding::same)
-       << convolutional_layer<activate>(16, 16, 3, 16, 16, padding::same)
-       << max_pooling_layer<activate>(16, 16, 16, 2)
-          //<< drop_01
-       << convolutional_layer<activate>(8, 8, 3, 16, 24, padding::same)
-       << convolutional_layer<activate>(8, 8, 3, 24, 24, padding::same)
-       << max_pooling_layer<activate>(8, 8, 24, 2)
-       << drop_02
-       << fully_connected_layer<softmax>(384, 2);
+    nn << convolutional_layer<activate>(32, 32, 3, 3, 32, padding::same)
+       << convolutional_layer<activate>(32, 32, 3, 32, 32, padding::same)
+       << max_pooling_layer<activate>(32, 32, 32, 2)
+       << dropout_layer(16*16*32, 0.25)
+       << convolutional_layer<activate>(16, 16, 3, 32, 64, padding::same)
+       << convolutional_layer<activate>(16, 16, 3, 64, 64, padding::same)
+       << max_pooling_layer<activate>(16, 16, 64, 2)
+       << dropout_layer(8*8*64, 0.25)
+       << fully_connected_layer<softmax>(8*8*64, 10);
 }
 
 
 }
 
 car_benchmark::car_benchmark()
-{    
-    using namespace ocv::ml;
+{            
+    load_car_data("train_cars", load_car_region("cars_train_annos.txt"),
+                  train_images_, train_labels_);
+    load_car_data("test_cars", load_car_region("cars_test_annos.txt"),
+                  test_images_, test_labels_);
 
-    std::vector<vec_t> in_img;
-    std::vector<label_t> in_label;
-    load_data("cars_train_small", 0,
-              in_img, in_label);
     load_data("non_cars", 1,
-              in_img, in_label);
-    std::tie(train_images_, train_labels_,
-             test_images_, test_labels_) =
-            split_train_test_inplace(in_img, in_label,
-                                     0.1, true);
+              train_images_, train_labels_);
+    load_data("test_non_cars", 1,
+              test_images_, test_labels_);//*/
+
+    /*parse_cifar10("cifar-10-batches-bin/data_batch_1.bin", &train_images_, &train_labels_, 0, 1, 0, 0);
+    parse_cifar10("cifar-10-batches-bin/data_batch_2.bin", &train_images_, &train_labels_, 0, 1, 0, 0);
+    parse_cifar10("cifar-10-batches-bin/data_batch_3.bin", &train_images_, &train_labels_, 0, 1, 0, 0);
+    parse_cifar10("cifar-10-batches-bin/data_batch_4.bin", &train_images_, &train_labels_, 0, 1, 0, 0);
+    parse_cifar10("cifar-10-batches-bin/data_batch_5.bin", &train_images_, &train_labels_, 0, 1, 0, 0);
+    parse_cifar10("cifar-10-batches-bin/test_batch.bin", &test_images_, &test_labels_, 0, 1, 0, 0);//*/
+
+    std::vector<std::pair<vec_t, label_t>> img_and_labels;
+    for(size_t i = 0; i != train_images_.size(); ++i){
+        img_and_labels.emplace_back(std::move(train_images_[i]),
+                                    train_labels_[i]);
+    }
+    std::random_device rd;
+    std::default_random_engine g(rd());
+    std::shuffle(std::begin(img_and_labels), std::end(img_and_labels), g);
+    for(size_t i = 0; i != train_images_.size(); ++i){
+        train_images_[i] = std::move(img_and_labels[i].first);
+        train_labels_[i] = img_and_labels[i].second;
+    }
+
     try{
         train_test();
     }catch(std::exception const &ex){
@@ -77,49 +105,80 @@ car_benchmark::car_benchmark()
     }
 }
 
+void car_benchmark::add_data(label_t label, cv::Mat const &img,
+                             Labels &labels, Imgs &imgs)
+{
+    if(!img.empty()){
+        cv::Mat resize_img;
+        cv::resize(img, resize_img, {32,32});
+        imgs.emplace_back(cvmat_to_img<vec_t>(resize_img, -1, 1));
+        labels.emplace_back(label);
+
+        //cv::flip(resize_img, resize_img, 0); //flip horizontal
+        //imgs.emplace_back(cvmat_to_img<vec_t>(resize_img, 0, 1));
+        //labels.emplace_back(label);
+    }
+}
+
+void car_benchmark::load_car_data(const std::string &folder,
+                                  std::map<std::string, cv::Rect> const &car_region,
+                                  Imgs &imgs, Labels &labels)
+{
+    auto const names = ocv::file::get_directory_files(folder);
+    int const expand_pix = 16;
+    for(auto const &name : names){
+        cv::Mat img = cv::imread(folder + "/" + name,
+                                 imread_modes);
+        auto rect = car_region.at(name);
+        rect.x = std::max(0, rect.x - expand_pix);
+        rect.width = std::min(rect.width + 2*expand_pix,
+                              img.cols - rect.x - 1);
+
+        rect.y = std::max(0, rect.y - expand_pix);
+        rect.height = std::min(rect.height + 2*expand_pix,
+                               img.rows - rect.y - 1);
+
+        add_data(0, img(rect), labels, imgs);
+    }
+}
+
+std::map<std::string, cv::Rect> car_benchmark::
+load_car_region(const std::string &file) const
+{
+    std::ifstream in(file);
+    int x1,x2,y1,y2;
+    std::string name;
+    std::map<std::string, cv::Rect> result;
+    while(!in.eof()){
+        in>>x1>>x2>>y1>>y2>>name;
+        result.insert({name, {x1, y1, x2-x1, y2-y1}});
+    }
+
+    return result;
+}
+
 void car_benchmark::load_data(std::string const &folder,
                               label_t label,
                               Imgs &imgs, Labels &labels)
 {
     auto const names = ocv::file::get_directory_files(folder);
-    cv::Mat aug_img;
     for(auto const &name : names){
-        try{
-            cv::Mat img = cv::imread(folder + "/" + name);
-            cv::resize(img, img, {32,32});
-            cv::cvtColor(img, img, CV_BGR2GRAY);
-            if(!img.empty()){
-                imgs.emplace_back(cvmat_to_img<vec_t>(img, 0, 1));
-                labels.emplace_back(label);
-
-                cv::flip(img, aug_img, 0); //flip horizontal
-                imgs.emplace_back(cvmat_to_img<vec_t>(aug_img, 0, 1));
-                labels.emplace_back(label);
-            }
-        }catch(std::exception const &ex){
-            std::cout<<(folder + "/" + name)<<" load fail"<<std::endl;
-            std::cout<<"load fail reason : "<<ex.what()<<std::endl;
-        }
+        cv::Mat img = cv::imread(folder + "/" + name,
+                                 imread_modes);
+        add_data(label, img, labels, imgs);
     }
-    std::cout<<"imgs size after loading : "<<imgs.size()<<std::endl;
 }
 
 void car_benchmark::train_test()
 {
-    network<mse, RMSprop> nn;
+    network<mse, adagrad> nn;
 
     create_lenet(nn);
 
-    std::cout << "start learning" << std::endl;
-
-    int const minibatch_size = 10;
+    int const minibatch_size = 20;
     int const num_epochs = 10;
 
     nn.optimizer().alpha *= std::sqrt(minibatch_size);
-    //int const minibatch_size = 5;
-    //int const num_epochs = 10;
-
-    //nn.optimizer().alpha *= std::sqrt(train_images_.size());
 
     ocv::tiny_cnn::trainer tt("car_weights", minibatch_size, num_epochs);
     tt.train_and_test(nn, train_images_, train_labels_,
--- tiny_cnn_test/car_benchmark.hpp
+++ tiny_cnn_test/car_benchmark.hpp
@@ -1,7 +1,11 @@
 #ifndef CAR_BENCHMARK_H
 #define CAR_BENCHMARK_H
 
+#include <opencv2/core.hpp>
+
 #include <tiny_cnn/tiny_cnn.h>
+
+#include <map>
 
 class car_benchmark
 {
@@ -12,6 +16,13 @@ private:
     using Imgs = std::vector<tiny_cnn::vec_t>;
     using Labels = std::vector<tiny_cnn::label_t>;
 
+    void add_data(tiny_cnn::label_t label, cv::Mat const &img,
+                  Labels &labels, Imgs &imgs);
+    void load_car_data(std::string const &folder,
+                       std::map<std::string, cv::Rect> const &car_region,
+                       Imgs &imgs, Labels &labels);
+    std::map<std::string, cv::Rect>
+    load_car_region(std::string const &file) const;
     void load_data(std::string const &folder,
                    tiny_cnn::label_t label,
                    Imgs &imgs, Labels &labels);
@@ -20,7 +31,7 @@ private:
     Imgs test_images_;
     Labels test_labels_;
     Imgs train_images_;
-    Labels train_labels_;
+    Labels train_labels_;    
 };
 
 #endif // CAR_DETECTION_H
