b665b6bb3e963cda942ab833fd0ebe5f84be8278
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-01-30 01:48:42 +0800

    change the way of reading color image

--- tiny_cnn_test/utility.hpp
+++ tiny_cnn_test/utility.hpp
@@ -18,10 +18,25 @@ Img cvmat_to_img(cv::Mat const& input,
     CV_Assert(scale_min < scale_max);
 
     Img img;
-    ocv::for_each_channels<uchar>(input, [&](uchar c)
-    {
-       img.emplace_back(scale_min + (scale_max - scale_min) * c / 255.0);
-    });
+    if(input.type() == CV_8UC3){
+        int const area = input.rows * input.cols;
+        img.resize(area*3);
+        for (int i = 0; i < input.rows; i++) { // Go over all rows
+            int const width = input.cols*i;
+            for (int j = 0; j < input.cols; j++) { // Go over all columns
+                for (int c = 0; c < 3; c++) { // Go through all channels
+                    img[area*c + width + j] =
+                            input.at<cv::Vec3b>(i, j)[c]/255.0*
+                            (scale_max - scale_min) + scale_min;
+                }
+            }
+        }
+    }else{
+        ocv::for_each_channels<uchar>(input, [&](uchar c)
+        {
+            img.emplace_back(scale_min + (scale_max - scale_min) * c / 255.0);
+        });
+    }
 
     return img;
 }
