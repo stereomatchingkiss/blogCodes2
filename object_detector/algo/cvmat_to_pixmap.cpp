c2d52dc717d587fea09dad41277cf3a5f6ade201
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-02-08 12:21:58 +0800

    do not convert to pixmap if input is empty

--- object_detector/algo/cvmat_to_pixmap.cpp
+++ object_detector/algo/cvmat_to_pixmap.cpp
@@ -17,20 +17,24 @@ cvmat_to_pixmap::cvmat_to_pixmap(QSize const &size) :
 
 QPixmap cvmat_to_pixmap::to_pixmap(cv::Mat input)
 {
-    if(input.rows > size_.height() ||
-            input.cols > size_.width()){
-        qDebug()<<this<<" : "<<size_;
-        auto const new_size = input.cols > input.rows ?
-                    cv::Size(size_.width(), 0) :
-                    cv::Size(0, size_.height());
+    if(!input.empty()){
+        if(input.rows > size_.height() ||
+                input.cols > size_.width()){
+            qDebug()<<this<<" : "<<size_;
+            auto const new_size = input.cols > input.rows ?
+                        cv::Size(size_.width(), 0) :
+                        cv::Size(0, size_.height());
 
-        ocv::resize_aspect_ratio(input, img_, new_size);
-        //std::cout<<"resize img size : "<<img_.size()<<std::endl;
-    }else{
-        img_ = input;
+            ocv::resize_aspect_ratio(input, img_, new_size);
+            //std::cout<<"resize img size : "<<img_.size()<<std::endl;
+        }else{
+            img_ = input;
+        }
+
+        return QPixmap::fromImage(ocv::qt::mat_to_qimage_ref(img_));
     }
 
-    return QPixmap::fromImage(ocv::qt::mat_to_qimage_ref(img_));
+    return {};
 }
 
 QPixmap cvmat_to_pixmap::to_pixmap(const QString &name)
