eb51d82009a6e04491162654675d1fccc7fac119
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-02-08 22:44:06 +0800

    fix bug--do not switch region icon properly

--- object_detector/ui/mainwindow.cpp
+++ object_detector/ui/mainwindow.cpp
@@ -28,8 +28,10 @@ MainWindow::MainWindow(QWidget *parent) :
     qDebug()<<maximum_size_;
     qDebug()<<ui->label_img->sizeHint();
     qDebug()<<size();
+    qDebug()<<ui->label_img->contentsRect();
     ui->comboBoxTasks->addItems(global::algo_list);
     ui->label_img->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+    ui->label_img->setScaledContents(false);
 
     mat_to_pixmap_ =
             std::make_unique<cvmat_to_pixmap>(maximum_size_);
@@ -52,15 +54,17 @@ void MainWindow::on_actionOpenFolder_triggered()
         file_names_ = dialog.selectedFiles();
         if(!file_names_.isEmpty()){
             ui->label_img->setPixmap(mat_to_pixmap_->to_pixmap(file_names_[0]));
-            ui->label_img->setFocus();
+            //ui->label_img->setPixmap(QPixmap(file_names_[0]));
+            label_has_focus_ = false;
+            ui->label_img->clearFocus();
+            ui->label_img->clear_rubber_band();
             has_image(true);
         }
     }
 }
 
 void MainWindow::on_actionStart_triggered()
-{
-
+{    
 }
 
 void MainWindow::on_actionStop_triggered()
@@ -80,12 +84,13 @@ void MainWindow::on_actionClearAll_triggered()
 
 void MainWindow::on_actionRegion_triggered()
 {
-    if(!label_has_focus_){
+    if(label_has_focus_){
         ui->actionRegion->setIcon(QIcon(":/pics/layer.png"));
-        ui->label_img->setFocus();
+        ui->label_img->clearFocus();
+        ui->label_img->clear_rubber_band();
     }else{
         ui->actionRegion->setIcon(QIcon(":/pics/layer_delete.png"));
-        ui->label_img->clearFocus();
+        ui->label_img->setFocus();
     }
     label_has_focus_ = !label_has_focus_;
 }
