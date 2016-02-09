f85d382d33e66fd1c47621177f0488c592a78406
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-02-09 08:30:10 +0800

    select object_classify_setting by default

--- object_detector/ui/setting_window.cpp
+++ object_detector/ui/setting_window.cpp
@@ -1,24 +1,40 @@
 //#include "general_setting.hpp"
+#include "object_classify_setting.hpp"
+
 #include "setting_window.hpp"
 #include "ui_setting_window.h"
 
 #include "../utility/global.hpp"
 
+#include <QDebug>
 #include <QStringListModel>
 
 setting_window::setting_window(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::setting_window)
+    ui(new Ui::setting_window),
+    obj_classify_setting_(new object_classify_setting(this))
 {
-    ui->setupUi(this);
+    ui->setupUi(this);    
+    ui->stackedWidgetSetting->addWidget(obj_classify_setting_);
     //ui->stackedWidgetSetting->addWidget(general_setting_);
 
     auto *model = new QStringListModel(this);
     model->setStringList(global::algo_list);
-    ui->listViewAlgo->setModel(model);    
+    ui->listViewAlgo->setModel(model);
+    ui->listViewAlgo->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->listViewAlgo->setCurrentIndex(model->index(0,0));
+
+    connect(ui->listViewAlgo, SIGNAL(clicked(QModelIndex)),
+            this, SLOT(algo_view_clicked(QModelIndex)));
 }
 
 setting_window::~setting_window()
 {
     delete ui;
+}
+
+void setting_window::algo_view_clicked(QModelIndex index)
+{
+    qDebug()<<"click on row "<<index.row();
+    ui->stackedWidgetSetting->setCurrentIndex(index.row());
 }
--- object_detector/ui/setting_window.hpp
+++ object_detector/ui/setting_window.hpp
@@ -2,12 +2,14 @@
 #define SETTING_WINDOW_HPP
 
 #include <QDialog>
+#include <QModelIndex>
 
 namespace Ui {
 class setting_window;
 }
 
 class general_setting;
+class object_classify_setting;
 
 class setting_window : public QDialog
 {
@@ -17,10 +19,14 @@ public:
     explicit setting_window(QWidget *parent = nullptr);
     ~setting_window();
 
+private slots:
+    void algo_view_clicked(QModelIndex index);
+
 private:
     Ui::setting_window *ui;
 
     //general_setting *general_setting_;
+    object_classify_setting *obj_classify_setting_;
 };
 
 #endif // SETTING_WINDOW_HPP
