accb2b6b78430b6b3c5e6a1d9a303de89ed22658
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-05-17 14:27:49 +0800

    able to set desire size, preprocess image and random shuffle the image

--- object_tracking/get_samples.cpp
+++ object_tracking/get_samples.cpp
@@ -9,37 +9,51 @@
 #include <opencv2/highgui.hpp>
 
 #include <iostream>
+#include <random>
 
 namespace{
 
 template<typename Reader>
 std::vector<cv::Mat>
-get_images(std::vector<std::string> const &folders,
-           std::string const &img_sub_folder)
+get_images(std::string const &folder,
+           std::string const &img_sub_folder,
+           size_t extract_size,
+           std::function<void(cv::Mat&)> preprocess,
+           unsigned int seed)
 {
     Reader reader;
     std::vector<cv::Mat> results;
+    std::vector<std::string> img_path;
 
-    for(auto &folder : folders){
-        auto maps = reader.parse_folder(folder);
-        auto const files =
-                ocv::file::get_directory_files(folder + "/" +
-                                               img_sub_folder);
-        for(auto const &file : files){
-            auto const img = cv::imread(folder + "/" +
-                                        img_sub_folder +
-                                        "/" + file);
-            if(!img.empty()){
-                auto it = maps.find(file);
-                if(it != std::end(maps)){
-                    auto const &rects = it->second;
-                    for(auto const &rect : rects){
-                        results.emplace_back(img(rect).clone());
-                    }
+    auto const files =
+            ocv::file::get_directory_files(folder + "/" +
+                                           img_sub_folder);
+    for(auto const &file : files){
+        img_path.emplace_back(file);
+    }
+
+    std::shuffle(std::begin(img_path), std::end(img_path),
+                 std::mt19937(seed));
+    img_path.resize(img_path.size() > extract_size ? extract_size :
+                                                     img_path.size());
+    auto const prefix = folder + "/" +
+            img_sub_folder + "/";
+    auto const maps = reader.parse_folder(folder);
+    for(auto const &name : img_path){
+        //std::cout<<name<<std::endl;
+        auto const img = cv::imread(prefix + name);
+        if(!img.empty()){
+            auto it = maps.find(name);
+            if(it != std::end(maps)){
+                auto const &rects = it->second;
+                for(auto const &rect : rects){
+                    auto region = img(rect).clone();
+                    preprocess(region);
+                    results.emplace_back(region);
                 }
             }
         }
-    }
+    }//*/
 
     return results;
 }
@@ -47,14 +61,27 @@ get_images(std::vector<std::string> const &folders,
 }
 
 std::vector<cv::Mat>
-get_stanford40_pose(const std::vector<std::string> &folders)
+get_stanford40_pose(std::string const &folder,
+                    size_t extract_size,
+                    std::function<void(cv::Mat&)> preprocess,
+                    unsigned int seed)
 {
-    return get_images<read_stanford40_pose>(folders,
-                                            "JPEGImages");
+    return get_images<read_stanford40_pose>(folder,
+                                            "JPEGImages",
+                                            extract_size,
+                                            preprocess,
+                                            seed);
 }
 
 std::vector<cv::Mat>
-get_usc_pedestrian(std::vector<std::string> const &folders)
+get_usc_pedestrian(std::string const &folder,
+                   size_t extract_size,
+                   std::function<void(cv::Mat&)> preprocess,
+                   unsigned int seed)
 {
-    return get_images<read_usc_pedestrian>(folders, "img");
+    return get_images<read_usc_pedestrian>(folder,
+                                           "img",
+                                           extract_size,
+                                           preprocess,
+                                           seed);
 }
--- object_tracking/get_samples.hpp
+++ object_tracking/get_samples.hpp
@@ -3,35 +3,52 @@
 
 #include <opencv2/core.hpp>
 
+#include <functional>
 #include <string>
 #include <vector>
 
 /**
  * Read the pose of stanford40
  * @param folders folders of stanford40
+ * @param extract_size number of sample retrieve, if the
+ * size larger than the dataset, this function will
+ * retrieve whole dataset
+ * @param preprocess preprocess the image
+ * @param seed seed for random shuffle
  * @return poses of stanford40
  * @code
  * * std::string const prefix();
- * get_usc_pedestrian({"../computer_vision_dataset/
- * human_pose/standord40"});
+ * get_usc_pedestrian("../computer_vision_dataset/
+ * human_pose/standord40");
  * @endcode
  */
 std::vector<cv::Mat>
-get_stanford40_pose(std::vector<std::string> const &folders);
+get_stanford40_pose(std::string const &folders,
+                    size_t extract_size,
+                    std::function<void(cv::Mat&)> preprocess,
+                    unsigned int seed = 0);
 
 /**
  * Get usc pedestrian.The organization of the data are
  * folder/(images)
  * folder/GT/(ground truth)
  * @param folders folders of usc
+ * @param extract_size number of sample retrieve, if the
+ * size larger than the dataset, this function will
+ * retrieve whole dataset
+ * @param preprocess preprocess the image
+ * @param seed seed for random shuffle
  * @return pedestrian of usc dataset
  * @code
  * std::string const prefix("../computer_vision_dataset/
  * pedestrian");
- * get_usc_pedestrian({prefix + "/USCPedestrianSetA"})
+ * get_usc_pedestrian(prefix + "/USCPedestrianSetA")
  * @endcode
  */
 std::vector<cv::Mat>
-get_usc_pedestrian(std::vector<std::string> const &folders);
+get_usc_pedestrian(std::string const &folders,
+                   size_t extract_size,
+                   std::function<void(cv::Mat&)> preprocess,
+                   unsigned int seed = 0);
 
 #endif //GET_SAMPLES_HPP
