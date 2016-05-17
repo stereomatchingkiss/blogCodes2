1cacea12997070bb7f38cdfe535f1780fc90acbc
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-05-17 13:00:17 +0800

    add function get_stanford40_pose

--- object_tracking/get_samples.cpp
+++ object_tracking/get_samples.cpp
@@ -1,4 +1,5 @@
 #include "get_samples.hpp"
+#include "read_stanford40_pose.hpp"
 #include "read_usc_pedestrian.hpp"
 
 #include <ocv_libs/file/utility.hpp>
@@ -9,21 +10,26 @@
 
 #include <iostream>
 
+namespace{
+
+template<typename Reader>
 std::vector<cv::Mat>
-get_usc_pedestrian(std::vector<std::string> const &folders)
+get_images(std::vector<std::string> const &folders,
+           std::string const &img_sub_folder)
 {
-    read_usc_pedestrian rup;    
+    Reader reader;
     std::vector<cv::Mat> results;
 
     for(auto &folder : folders){
-        auto maps = rup.parse_folder(folder);
+        auto maps = reader.parse_folder(folder);
         auto const files =
-                ocv::file::get_directory_files(folder + "/img");
-        for(auto const &file : files){            
-            //std::cout<<folder + "/img/" + file<<std::endl;
-            auto const img = cv::imread(folder + "/img/" + file,
-                                        cv::IMREAD_GRAYSCALE);
-            if(!img.empty()){                
+                ocv::file::get_directory_files(folder + "/" +
+                                               img_sub_folder);
+        for(auto const &file : files){
+            auto const img = cv::imread(folder + "/" +
+                                        img_sub_folder +
+                                        "/" + file);
+            if(!img.empty()){
                 auto it = maps.find(file);
                 if(it != std::end(maps)){
                     auto const &rects = it->second;
@@ -36,4 +42,19 @@ get_usc_pedestrian(std::vector<std::string> const &folders)
     }
 
     return results;
+}
+
+}
+
+std::vector<cv::Mat>
+get_stanford40_pose(const std::vector<std::string> &folders)
+{
+    return get_images<read_stanford40_pose>(folders,
+                                            "JPEGImages");
+}
+
+std::vector<cv::Mat>
+get_usc_pedestrian(std::vector<std::string> const &folders)
+{
+    return get_images<read_usc_pedestrian>(folders, "img");
 }
--- object_tracking/get_samples.hpp
+++ object_tracking/get_samples.hpp
@@ -7,6 +7,19 @@
 #include <vector>
 
 /**
+ * Read the pose of stanford40
+ * @param folders folders of stanford40
+ * @return poses of stanford40
+ * @code
+ * * std::string const prefix();
+ * get_usc_pedestrian({"../computer_vision_dataset/
+ * human_pose/standord40"});
+ * @endcode
+ */
+std::vector<cv::Mat>
+get_stanford40_pose(std::vector<std::string> const &folders);
+
+/**
  * Get usc pedestrian.The organization of the data are
  * folder/(images)
  * folder/GT/(ground truth)
