3bfa6e1573d3ce1a0328c76d66bc83cbbc6a0826
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-05-25 22:08:34 +0800

    add link of stack overflow

--- forum_quest/hough_lines_polygon.hpp
+++ forum_quest/hough_lines_polygon.hpp
@@ -1,6 +1,10 @@
 #ifndef HOUGH_LINES_POLYGON_HPP
 #define HOUGH_LINES_POLYGON_HPP
 
+/**
+ * solve the question from stack overflow--
+ * http://stackoverflow.com/questions/31036917/hough-line-transform-to-find-polygons-in-an-image
+ */
 void hough_lines_polygon_detection();
 
 #endif
