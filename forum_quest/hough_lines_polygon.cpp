c77f2a09b6077caf2be6bb7ab6d382aa34fd2e7c
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-05-25 22:17:10 +0800

    add comments

--- forum_quest/hough_lines_polygon.cpp
+++ forum_quest/hough_lines_polygon.cpp
@@ -47,30 +47,41 @@ void morphology_skeleton(cv::Mat &input, cv::Mat &output,
 
 void detect_by_contour()
 {
+    //Following comments are written for non c++ programmers
     auto img = cv::imread("../forum_quest/data/yd8pA.png");
     if(img.empty()){
         throw std::runtime_error("cannot open image");
     }
 
-
     cv::Mat gray_img;
     cv::cvtColor(img, gray_img, CV_BGR2GRAY);
     cv::Mat thin_img;
+    //make your lines as thin as possible
     morphology_skeleton(gray_img, thin_img);
 
     std::vector<std::vector<cv::Point>> contours;
     cv::findContours(thin_img, contours, cv::RETR_EXTERNAL,
                      cv::CHAIN_APPROX_SIMPLE);
+    //remove contour if the area less than 100
     auto it = std::remove_if(std::begin(contours), std::end(contours),
                    [](std::vector<cv::Point> const &a)
     {
         return cv::boundingRect(a).area() < 100;
     });
+    //remove_if move unwanted elements to the backyard of the containers
+    //you need to call the erase function of the containers to remove
+    //unwanted elements
     contours.erase(it, std::end(contours));
+
+    //contour_analyzer is a class used to print out statistic info
+    //of the contour
     ocv::contour_analyzer analyzer;
+    //print_contour_attribute_name print out the attribute names
+    //of the contours as following
+    //CArea   |   BArea   | Perimeter |   Aspect  |   Extent  |  Solidity |  PolySize
     ocv::print_contour_attribute_name(std::cout);
     for(size_t i = 0; i != contours.size(); ++i){
-        cv::drawContours(img, contours, static_cast<int>(i), {0,255,0}, 2);
+        cv::drawContours(img, contours, static_cast<int>(i), {0,255,0}, 2);        
         std::cout<<analyzer.analyze(contours[i], 0.1);
         cv::imshow("img", img);
         cv::waitKey();
