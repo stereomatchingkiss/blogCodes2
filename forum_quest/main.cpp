011b21d1cde7b50ca621882d65520ef97a20e61a
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-05-25 21:59:24 +0800

    test hough_lines_polygon

--- forum_quest/main.cpp
+++ forum_quest/main.cpp
@@ -1,6 +1,15 @@
+#include "hough_lines_polygon.hpp"
 #include "so_obj_detect_00.hpp"
+
+#include <exception>
+#include <iostream>
 
 int main()
 {
-   identify_objects_sequentially();
+    try{
+        hough_lines_polygon_detection();
+        //identify_objects_sequentially();
+    }catch(std::exception const &ex){
+        std::cerr<<ex.what()<<"\n";
+    }
 }
