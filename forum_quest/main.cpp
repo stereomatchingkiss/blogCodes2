ea07275285c21d5245980caab15ca593ca85e0ef
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-07-04 16:22:04 +0800

    run mapping_lines quest

--- forum_quest/main.cpp
+++ forum_quest/main.cpp
@@ -1,4 +1,5 @@
 #include "hough_lines_polygon.hpp"
+#include "mapping_files.hpp"
 #include "so_obj_detect_00.hpp"
 
 #include <exception>
@@ -7,7 +8,8 @@
 int main()
 {
     try{
-        hough_lines_polygon_detection();
+        //hough_lines_polygon_detection();
+        mapping_files();
         //identify_objects_sequentially();
     }catch(std::exception const &ex){
         std::cerr<<ex.what()<<"\n";
