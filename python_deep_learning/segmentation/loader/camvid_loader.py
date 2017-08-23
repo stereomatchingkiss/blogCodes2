1eda28e90c3241b73a19ce1fbcf39c330c1103f3
stereomatchingkiss <thamngapwei@gmail.com>
2017-08-24 07:33:11 +0800

    add class data_loader

--- python_deep_learning/segmentation/loader/camvid_loader.py
+++ python_deep_learning/segmentation/loader/camvid_loader.py
@@ -30,4 +30,69 @@ def read_color_count(file_location):
     return sorted(color_count, key=lambda color: color[3], reverse = True)
 
 
+class data_loader(Dataset):
+    """This class load data of image segmentation and return raw image and label in a dictionary. 
+    If cache is true, this class will save all of the images(including raws and labels) into
+    the ram, else it would try to load the image whenever __get_item__ is called
+    
+    Example:
+    loader = data_loader("/home/computer_vision_dataset/segmentation/camvid/train_raws/", "/home/computer_vision_dataset/segmentation/camvid/train_labels/", None, False)
+    sample = loader[0]
+    raw_img = sample['raw']
+    label_img = sample['label']
+    """
+    
+    def __init__(self, raw_folder, label_folder, transform = None, cache = True):
+        super(data_loader, self).__init__()
+                        
+        self._mcache = cache
+        self._mtransform = transform
+        
+        self._mraw_names, self._mraw_imgs, self._mlabel_names, self._mlabel_imgs = self._load_data(raw_folder, label_folder, cache)        
+        print(len(self._mraw_names), len(self._mraw_imgs))
+        print(len(self._mlabel_names), len(self._mlabel_imgs))                
+        
+    def __getitem__(self, idx):
+        """This function return two data, raw image and label image        
+        """
+
+        result = {}
+        if(self._mcache):
+            result = {'raw' : self._mraw_imgs[idx], 'label' : self._mlabel_imgs[idx]}
+        else:            
+            print(self._mraw_names[idx], self._mlabel_names[idx])
+            result = {'raw' : np.array(pil.Image.open(self._mraw_names[idx])), 'label' : np.array(pil.Image.open(self._mlabel_names[idx]))}
+            
+        if self._mtransform:
+            result = self._mtransform(result)
+            
+        return result
+        
+    def __len__(self):
+        return len(self._mraw_names)
+            
+    def _load_data(self, raw_folder, label_folder, cache):
+        
+        raw_names = []
+        raw_imgs = []
+        label_names = []
+        label_imgs = []
+        valid_types = ['jpg', 'jpeg', 'png', 'bmp']        
+        for root, dirs, files in os.walk(raw_folder):
+            for i, f in enumerate(files):
+                label_name = f[0:-4] + "_L.png"                
+                raw_type = imghdr.what(raw_folder + f)                
+                if raw_type in valid_types:
+                    print(i, "is valid type:", "open raw file:", raw_folder + f, "open label file:", label_folder + label_name)
+                    raw_names.append(raw_folder + f)
+                    label_names.append(label_folder + label_name)
+                    if cache:
+                        img = pil.Image.open(raw_folder + f)
+                        raw_imgs.append(np.array(img))
+                        img = pil.Image.open(label_folder + label_name)
+                        label_imgs.append(np.array(img))
+                                        
+        return raw_names, raw_imgs, label_names, label_imgs
+
+
 
