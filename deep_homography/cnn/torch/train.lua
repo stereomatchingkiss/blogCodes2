0c278dfe549bd96d88e202f214f31fa077425489
stereomatchingkiss <thamngapwei@gmail.com>
2017-10-08 17:24:20 +0800

    1 : Able to parse command line arguments 2 : Save the network when reaching loss_record_step

--- deep_homography/cnn/torch/train.lua
+++ deep_homography/cnn/torch/train.lua
@@ -8,8 +8,32 @@ require 'paths'
 require 'squeeze_net'
 require 'utils'
 
-folder = "/home/ramsus/Qt/blogCodes2/deep_homography/data/ms_coco_train_gray_10000/"
---folder = "/home/ramsus/Qt/blogCodes2/deep_homography/data/ms_coco_train_gray_500032/"
+cmd = torch.CmdLine()
+cmd:text('Options')
+cmd:option('-folder','../../data/ms_coco_train_gray_10000','Folder contain images and info.txt')
+cmd:option('-batch_size',100,'Batch size of training')
+cmd:option('-loss_record_step',100,'How many steps the program should record training results')
+cmd:option('-total_steps', 500, 'Total training steps')
+
+params = cmd:parse(arg)
+
+folder = params['folder']
+
+local function deep_copy(tbl)
+   -- creates a copy of a network with new modules and the same tensors
+   local copy = {}
+   for k, v in pairs(tbl) do
+      if type(v) == 'table' then
+         copy[k] = deep_copy(v)
+      else
+         copy[k] = v
+      end
+   end
+   if torch.typename(tbl) then
+      torch.setmetatable(copy, torch.typename(tbl))
+   end
+   return copy
+end
 
 function train(batch_size, loss_record_step, total_steps)
         
@@ -25,21 +49,20 @@ function train(batch_size, loss_record_step, total_steps)
     
     local params, gradParams = net:getParameters()
     local momentum_val = 0.9
-    --local optimState = {learningRate = 0.005, momentum = momentum_val}
-    local optimState = {learningRate = 0.005}
+    local optimState = {learningRate = 0.005, momentum = momentum_val}    
     
     local file = io.open('loss_record.txt', 'w')
     
     local function feval(params)
         return criterion.output, gradParams
     end            
-    
-    local total_loss = 0
+            
+    local total_loss = 0    
     local start_time = os.time()
     for i = 1, total_steps do             
         gradParams:zero()
         local offset = ((i-1) * batch_size) % img_len
-        print(folder, img_len, offset + 1, offset + batch_size)
+        --print(folder, img_len, offset + 1, offset + batch_size)
         local input_tensor = read_imgs(imgs_name, folder, offset + 1, offset + batch_size):cuda()
         input_tensor = input_tensor / 255.0
         local labels = delta[{ {offset + 1, offset + batch_size}, {} }]
@@ -48,6 +71,8 @@ function train(batch_size, loss_record_step, total_steps)
         local loss = criterion:forward(predict_output, labels)
         local dloss = criterion:backward(predict_output, labels)
         net:backward(input_tensor, dloss)
+        
+        optim.sgd(feval, params, optimState)
                 
         total_loss = total_loss + loss                        
         print(i, ':loss = ', loss)        
@@ -55,17 +80,15 @@ function train(batch_size, loss_record_step, total_steps)
             print(i, ':total loss = ', total_loss/loss_record_step)
             file:write(i .. ':total loss = ' .. total_loss/loss_record_step .. '\n')
             total_loss = 0
+            local model = deep_copy(net):float():clearState()
+            torch.save('squeeze_homo_' .. i, model)
         end
         
         if i == 30000 then 
-            --optimState = {learningRate = 0.0005, momentum = momentum_val}
-            optimState = {learningRate = 0.0005}
+            optimState = {learningRate = 0.0005, momentum = momentum_val}            
         elseif i == 60000 then
-            --optimState = {learningRate = 0.00005, momentum = momentum_val}
-            optimState = {learningRate = 0.00005}
-        end
-        
-        optim.sgd(feval, params, optimState)
+            optimState = {learningRate = 0.00005, momentum = momentum_val}            
+        end                
     end   
     
     local end_time = os.time()
@@ -74,9 +97,8 @@ function train(batch_size, loss_record_step, total_steps)
     
     net:clearState()
     net = net:float()
-    torch.save('squeeze_homo', net)    
+    torch.save('squeeze_homo', net)--]]    
     
 end    
 
---train(10, 10000, 90000)
-train(100, 100, 500)
+train(params['batch_size'], params['loss_record_step'], params['total_steps'])
