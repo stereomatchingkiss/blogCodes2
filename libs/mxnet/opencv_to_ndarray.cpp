7b914f79831458b63f758010b4710da34e784ea4
tham <thamngapwei@gmail.com>
2019-01-29 21:36:13 +0800

    first commit

---
+++ libs/mxnet/opencv_to_ndarray.cpp
@@ -0,0 +1,39 @@
+#include "opencv_to_ndarray.hpp"
+
+#include <opencv2/imgproc.hpp>
+
+#include <mxnet-cpp/MxNetCpp.h>
+
+using namespace mxnet::cpp;
+
+namespace mxnet_aux {
+
+opencv_to_ndarray::opencv_to_ndarray(int width,
+                                     int height,
+                                     Context const &context,
+                                     int convert_to,
+                                     bool switch_channel) :
+    context_(context),
+    convert_to_(convert_to),
+    size_(width, height),
+    switch_channel_(switch_channel)
+{
+
+}
+
+mxnet::cpp::NDArray opencv_to_ndarray::convert(cv::Mat const &input)
+{
+    cv::resize(input, resize_img_, size_);
+    if(switch_channel_){
+        cv::cvtColor(resize_img_, resize_img_, CV_BGR2RGB);
+    }
+    resize_img_.convertTo(float_img_, convert_to_);
+
+    return NDArray(float_img_.ptr<float>(),
+                   Shape(1, static_cast<unsigned>(size_.height), static_cast<unsigned>(size_.width), 3),
+                   context_);
+}
+
+
+
+}
---
+++ libs/mxnet/opencv_to_ndarray.hpp
@@ -0,0 +1,43 @@
+#ifndef OPENCV_TO_NDARRAY_HPP
+#define OPENCV_TO_NDARRAY_HPP
+
+#include <mxnet-cpp/MxNetCpp.h>
+
+#include <opencv2/core.hpp>
+
+#include <memory>
+
+namespace mxnet_aux{
+
+/**
+ * A more efficient way to convert Mat to NDArray since it will reuse allocated memory
+ */
+class opencv_to_ndarray
+{
+public:
+    /**
+     * @param width Width of NDArray
+     * @param height Height of NDArray
+     * @param convert_to Ex : CV_32FC3
+     * @param switch_channel : True will switch R and B channel
+     */
+    opencv_to_ndarray(int width,
+                      int height,
+                      mxnet::cpp::Context const &context,
+                      int convert_to = CV_32FC3,
+                      bool switch_channel = true);
+
+    mxnet::cpp::NDArray convert(cv::Mat const &input);
+
+private:
+    mxnet::cpp::Context context_;
+    int convert_to_;
+    cv::Mat float_img_;
+    cv::Mat resize_img_;
+    cv::Size size_;
+    bool switch_channel_;
+};
+
+}
+
+#endif // OPENCV_TO_NDARRAY_HPP
