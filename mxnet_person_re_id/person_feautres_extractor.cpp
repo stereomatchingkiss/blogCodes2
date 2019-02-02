6e9c76cb14f7652def374a65cc75276d38a43d74
tham <thamngapwei@gmail.com>
2019-02-02 10:18:53 +0800

    first commit

---
+++ mxnet_person_re_id/person_feautres_extractor.cpp
@@ -0,0 +1,41 @@
+#include "person_feautres_extractor.hpp"
+
+#include <mxnet-cpp/MxNetCpp.h>
+
+#include <opencv2/imgproc.hpp>
+
+#include <libs/mxnet/common.hpp>
+#include <libs/mxnet/opencv_to_ndarray.hpp>
+
+using namespace mxnet::cpp;
+using namespace mxnet_aux;
+
+namespace{
+
+int constexpr input_height = 384;
+int constexpr input_width = 128;
+
+}
+
+person_feautres_extractor::person_feautres_extractor(const std::string &model_params,
+                                                     const std::string &model_symbols,
+                                                     const mxnet::cpp::Context &context)
+{
+    executor_ = create_executor(model_params, model_symbols, context,
+                                Shape(1, input_height, input_width, 3));
+    to_ndarray_ = std::make_unique<opencv_to_ndarray>(input_width, input_height, context, CV_32FC3, false);
+}
+
+NDArray person_feautres_extractor::get_features(const cv::Mat &input)
+{
+    auto data = to_ndarray_->convert(input);
+    data.CopyTo(&executor_->arg_dict()["data"]);
+    executor_->Forward(false);
+
+    if(!executor_->outputs.empty()){
+        return executor_->outputs[0];
+    }
+
+    return {};
+}
+
