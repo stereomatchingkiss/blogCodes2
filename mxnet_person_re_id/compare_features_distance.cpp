cf54af057fe3dac4c11cde42b2a55640798e507e
tham <thamngapwei@gmail.com>
2019-02-02 19:31:31 +0800

    remove unused function

--- mxnet_person_re_id/compare_features_distance.cpp
+++ mxnet_person_re_id/compare_features_distance.cpp
@@ -6,17 +6,6 @@ using namespace mxnet::cpp;
 
 namespace{
 
-float ndarray_sum(const mxnet::cpp::NDArray &input, size_t size = 2048)
-{
-    float sum = 0;
-    auto *data = input.GetData();
-    for(size_t i = 0; i != size; ++i){
-        sum += data[i];
-    }
-
-    return sum;
-}
-
 float element_wise_multiply_sum(mxnet::cpp::NDArray const &lhs, mxnet::cpp::NDArray const &rhs, size_t size = 2048)
 {
     float sum = 0;
