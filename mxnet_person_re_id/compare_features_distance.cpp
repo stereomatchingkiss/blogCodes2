cbe0c51e8d5f33de0460cf65bd17b0c1431040ff
tham <thamngapwei@gmail.com>
2019-02-02 20:58:59 +0800

    add new api and make old api easier to use

--- mxnet_person_re_id/compare_features_distance.cpp
+++ mxnet_person_re_id/compare_features_distance.cpp
@@ -20,13 +20,30 @@ float element_wise_multiply_sum(mxnet::cpp::NDArray const &lhs, mxnet::cpp::NDAr
 
 }
 
-float cosine_similarity::compare_feature(mxnet::cpp::NDArray const &lhs, mxnet::cpp::NDArray const &rhs) const
+float cosine_similarity::compare_feature(mxnet::cpp::NDArray const &lhs,
+                                         mxnet::cpp::NDArray const &rhs,
+                                         size_t size) const
 {
-    auto const numerator = element_wise_multiply_sum(lhs, rhs);
-    auto const denom_lhs = element_wise_multiply_sum(lhs, lhs);
-    auto const denom_rhs = element_wise_multiply_sum(rhs, rhs);
+    auto const numerator = element_wise_multiply_sum(lhs, rhs, size);
+    auto const denom_lhs = element_wise_multiply_sum(lhs, lhs, size);
+    auto const denom_rhs = element_wise_multiply_sum(rhs, rhs, size);
 
     auto const denominator = std::sqrt(denom_lhs * denom_rhs);
 
     return denominator != 0.0f ? numerator / denominator : 0.0f;
+}
+
+float cosine_similarity::compare_feature(const cv::Mat_<float> &lhs, const cv::Mat_<float> &rhs) const
+{
+    cv::multiply(lhs, rhs, numerator_);
+    cv::pow(lhs, 2, lhs_pow_);
+    cv::pow(rhs, 2, rhs_pow_);
+
+    auto const numerator = cv::sum(numerator_)[0];
+    auto const denom_lhs = cv::sum(lhs_pow_)[0];
+    auto const denom_rhs = cv::sum(rhs_pow_)[0];
+
+    auto const denominator = std::sqrt(denom_lhs * denom_rhs);
+
+    return static_cast<float>(denominator != 0.0 ? numerator / denominator : 0.0);
 }
--- mxnet_person_re_id/compare_features_distance.hpp
+++ mxnet_person_re_id/compare_features_distance.hpp
@@ -1,22 +1,28 @@
 #ifndef COMPARE_FEATURES_DISTANCE_HPP
 #define COMPARE_FEATURES_DISTANCE_HPP
 
+#include <opencv2/core.hpp>
+
 namespace mxnet
 {
-
 namespace cpp
 {
 
 class NDArray;
 
 }
-
 }
 
 class cosine_similarity
 {
 public:
-    float compare_feature(mxnet::cpp::NDArray const &lhs, mxnet::cpp::NDArray const &rhs) const;
+    float compare_feature(mxnet::cpp::NDArray const &lhs, mxnet::cpp::NDArray const &rhs, size_t size = 2048) const;
+    float compare_feature(cv::Mat_<float> const &lhs, cv::Mat_<float> const &rhs) const;
+
+private:
+    cv::Mat_<float> lhs_pow_;
+    cv::Mat_<float> numerator_;
+    cv::Mat_<float> rhs_pow_;
 };
 
 #endif // COMPARE_FEATURES_DISTANCE_HPP
