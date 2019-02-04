a4dbf2c4632a384c8f7afa6c5f220d4fc9f2fd44
tham <thamngapwei@gmail.com>
2019-02-04 23:42:04 +0800

    first commit

---
+++ mxnet_person_re_id/main.cpp
@@ -0,0 +1,63 @@
+#include "visitor_identify.hpp"
+
+#include <libs/mxnet/common.hpp>
+
+#include <mxnet-cpp/MxNetCpp.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/video.hpp>
+
+#include <iostream>
+#include <string>
+
+using namespace mxnet_aux;
+using namespace std;
+
+cv::VideoCapture create_video_capture(cv::FileStorage const &input)
+{
+    if(input["input_media"].isInt()){
+        return cv::VideoCapture(static_cast<int>(input["input_media"].real()));
+    }
+
+    return cv::VideoCapture(input["input_media"].string());
+}
+
+int main(int argc, char *argv[])try
+{
+    if(argc < 2){
+        cout<<"Must specify location of json. ex : mxnet_person_re_id settings.json"<<endl;
+        return -1;
+    }
+
+    cv::FileStorage fs(argv[1], cv::FileStorage::READ);
+    if(fs.isOpened()){
+        auto capture = create_video_capture(fs);
+        if(capture.isOpened()){
+            auto const person_re_id_params = fs["person_re_id_params"].string();
+            auto const person_re_id_symbol = fs["person_re_id_symbol"].string();
+            auto const obj_det_params = fs["obj_det_params"].string();
+            auto const obj_det_symbols = fs["obj_det_symbols"].string();
+            visitor_identify viden(person_re_id_params, person_re_id_symbol, obj_det_params, obj_det_symbols);
+            cv::Mat frame;
+            while(1){
+                capture>>frame;
+                if(!frame.empty()){
+                    auto const visitor_info = viden.detect_and_identify_visitors(frame);
+                    cv::imshow("frame", frame);
+                    auto const key = cv::waitKey(10);
+                    if(key == 'q' || key == 'Q'){
+                        break;
+                    }
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+
+    return 0;
+}catch(std::exception const &ex){
+    cerr<<ex.what()<<endl;
+}
---
+++ mxnet_person_re_id/mxnet_person_re_id.pro
@@ -0,0 +1,36 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+INCLUDEPATH += ../
+
+OPENCV_PATH = ../../3rdLibs/opencv/opencv_3_4_2/opencv/build
+MXNET_PATH = ../../3rdLibs/incubator-mxnet/build/install
+
+INCLUDEPATH += $${OPENCV_PATH}/include
+INCLUDEPATH += $${MXNET_PATH}/include
+
+LIBS += $${OPENCV_PATH}/x64/vc14/lib/opencv_world342.lib
+LIBS += $${MXNET_PATH}/lib/libmxnet.lib
+
+SOURCES += \
+        main.cpp \    
+    ../libs/mxnet/common.cpp \
+    ../mxnet_cpp_object_detection/object_detector.cpp \
+    ../mxnet_cpp_object_detection/object_detector_filter.cpp \
+    ../libs/mxnet/opencv_to_ndarray.cpp \
+    compare_features_distance.cpp \
+    re_id_db.cpp \
+    visitor_identify.cpp \
+    person_feautres_extractor.cpp
+
+HEADERS += \    
+    ../libs/mxnet/common.hpp \
+    ../mxnet_cpp_object_detection/object_detector.hpp \
+    ../mxnet_cpp_object_detection/object_detector_filter.hpp \
+    ../libs/mxnet/opencv_to_ndarray.hpp \
+    compare_features_distance.hpp \
+    re_id_db.hpp \
+    visitor_identify.hpp \
+    person_feautres_extractor.hpp
