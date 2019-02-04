e444182b1f7a070b44ce4fd534483df331f545d0
tham <thamngapwei@gmail.com>
2019-02-04 23:26:00 +0800

    make class easier to use

--- mxnet_person_re_id/re_id_db.cpp
+++ mxnet_person_re_id/re_id_db.cpp
@@ -2,24 +2,11 @@
 
 #include "compare_features_distance.hpp"
 
-#include <libs/mxnet/common.hpp>
-#include <libs/mxnet/opencv_to_ndarray.hpp>
-
-#include <mxnet-cpp/MxNetCpp.h>
-
-using namespace mxnet::cpp;
 using namespace mxnet_aux;
 
-re_id_db::re_id_db(const std::string &model_params,
-                   const std::string &model_symbols,
-                   const mxnet::cpp::Context &context) :
-    compare_(std::make_unique<cosine_similarity>()),
-    to_ndarray_(std::make_unique<mxnet_aux::opencv_to_ndarray>(128, 384, context))
-{
-    executor_ = create_executor(model_params,
-                                model_symbols,
-                                context,
-                                Shape(1, 128, 384, 3));
+re_id_db::re_id_db() :
+    compare_(std::make_unique<cosine_similarity>())
+{    
 }
 
 re_id_db::~re_id_db()
@@ -27,21 +14,15 @@ re_id_db::~re_id_db()
 
 }
 
-size_t re_id_db::add_new_id(const cv::Mat &input)
+size_t re_id_db::add_new_id(cv::Mat_<float> const &input)
 {
-    auto data = to_ndarray_->convert(input);
-    data.CopyTo(&executor_->arg_dict()["data"]);
-    executor_->Forward(false);
-    cv::Mat_<float> feature(1, 2048);
-    executor_->outputs[0].SyncCopyToCPU(reinterpret_cast<mx_float*>(feature.data), 2048);
-    features_.emplace_back(feature);
-
-    return features_.size();
+    features_.emplace_back(input);
+    return features_.size() - 1;
 }
 
-re_id_db::id_info re_id_db::find_most_similar_id(const cv::Mat &input) const
+re_id_db::id_info re_id_db::find_most_similar_id(cv::Mat_<float> const &input)
 {
-    id_info result;
+    id_info result;    
     for(size_t i = 0; i != features_.size(); ++i){
         auto const confident = compare_->compare_feature(features_[i], input);
         if(confident > result.confident_){
--- mxnet_person_re_id/re_id_db.hpp
+++ mxnet_person_re_id/re_id_db.hpp
@@ -32,23 +32,19 @@ class re_id_db
 public:
     struct id_info
     {
-        float confident_ = -2.0f;
+        float confident_ = -2.0f;        
         size_t id_ = 0;
     };
 
-    re_id_db(std::string const &model_params,
-             std::string const &model_symbols,
-             mxnet::cpp::Context const &context);
+    re_id_db();
     ~re_id_db();
 
-    size_t add_new_id(cv::Mat const &input);
-    id_info find_most_similar_id(cv::Mat const &input) const;
+    size_t add_new_id(cv::Mat_<float> const &input);
+    id_info find_most_similar_id(cv::Mat_<float> const &input);
 
 private:
-    std::unique_ptr<cosine_similarity> compare_;
-    std::unique_ptr<mxnet::cpp::Executor> executor_;
-    std::vector<cv::Mat_<float>> features_;
-    std::unique_ptr<mxnet_aux::opencv_to_ndarray> to_ndarray_;
+    std::unique_ptr<cosine_similarity> compare_;    
+    std::vector<cv::Mat_<float>> features_;    
 };
 
 #endif // RE_ID_DB_HPP
