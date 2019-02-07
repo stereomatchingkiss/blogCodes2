8a840cf1c5ebdfdd10243155a9e7da133691dcca
tham <thamngapwei@gmail.com>
2019-02-07 12:16:31 +0800

    change class name and make it easier to use

--- mxnet_and_multi_label/MultiLabelImageDataset.py
+++ mxnet_and_multi_label/MultiLabelImageDataset.py
@@ -2,30 +2,37 @@ import mxnet as mx
 from mxnet.gluon.data import dataset
 import os
 
-class MultiLabelImageDataset(dataset.Dataset):
+class MultiOutputImageDataset(dataset.Dataset):
     """
-    A dataset for loading images with multi labels. 
+    A dataset for loading images with multi outputs. 
 	
 	1. Each label should be separated by "_"
-	2. The images are store as 
+    2. Same category should put at the same order
+    ex : red_shoe, blue_shirt, black_jean
+	3. The folders architectures
 	
-	label0_label1_label2
+	red_shirt
 	  --img0.jpg
 	  --img1.jpg
-	label1_label4_label5
-	  --img4.jpg
-	  --img5.jpg
-	....
-	....
-	....		
+	red_jean
+	  --img0.jpg
+	  --img1.jpg
+	blue_shirt
+      --img0.jpg
+	blue_jean
+	  --img0.jpg
 		
-	3.  The results returned by __getitem__ will be
-	path of image     number after mapping
+	4.  Example
+    
+    MultiOutputImageDataset("./images", [{"red" : 0, "blue" : 1}, {"shirt" : 0, "jean" : 1}])
+
+    The results returned by __getitem__ will be
+	path of image               number after mapping
 	
-	root/img0.jpg     [0, 1]
-	root/img1.jpg     [0, 1]
-	root/img2.jpg     [2, 3]
-	root/img3.jpg     [2, 3]	
+	root/red_shirt/img0.jpg     [0, 0]
+	root/red_jean/img1.jpg      [0, 1]
+	root/blue_shirt/img2.jpg    [1, 1]
+	root/blue_jean/img3.jpg     [1, 0]	
 
     Parameters
     ----------
@@ -36,17 +43,16 @@ class MultiLabelImageDataset(dataset.Dataset):
     ::
         transform = lambda data, label: (data.astype(np.float32)/255, label)
     """
-    def __init__(self, root, transform=None):
+    def __init__(self, root, label_dict, transform=None):
         self._root = os.path.expanduser(root)
+        self._label_dict = label_dict
         self._transform = transform
         self._exts = ['.jpg', '.jpeg', '.png']
         self._list_images(self._root)
 
     def _list_images(self, root):
-        self._image_list = []
-        self._label_dict = {}
-        self._label_list = []
-        label_index = 0;
+        self._image_list = []        
+        self._label_list = []        
         for dir in os.listdir(root):                         
             for filename in os.listdir(root + "/" + dir):
                 name, ext = os.path.splitext(filename)
@@ -55,11 +61,8 @@ class MultiLabelImageDataset(dataset.Dataset):
                 else:
                     labels = dir.split("_")
                     num_of_labels = []
-                    for l in labels:
-                        if l not in self._label_dict:					
-                            self._label_dict[l] = label_index
-                            label_index = label_index + 1
-                        num_of_labels.append(self._label_dict[l])
+                    for i in range(labels):
+                        num_of_labels.append(self._label_dict[i][labels[i]])
                     self._image_list.append(root + "/" + dir + "/" + filename)
                     self._label_list.append(num_of_labels)
 	                                        
