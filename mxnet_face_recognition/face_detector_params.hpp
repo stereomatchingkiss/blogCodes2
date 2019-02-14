528155cf6c83e01ffd95c6e1dfd2170682a3f913
tham <thamngapwei@gmail.com>
2019-02-14 13:24:48 +0800

    first commit

---
+++ mxnet_face_recognition/face_detector_params.hpp
@@ -0,0 +1,25 @@
+#ifndef FACE_DETECTOR_PARAMS_HPP
+#define FACE_DETECTOR_PARAMS_HPP
+
+#include <string>
+
+namespace dlib_tool
+{
+
+struct face_detector_params
+{
+    face_detector_params(std::string face_detect_model_location,
+                         std::string shape_predict_model_location) :
+        face_detect_model_location_(std::move(face_detect_model_location)),
+        shape_predict_model_location_(std::move(shape_predict_model_location))
+    {}
+
+    std::string face_detect_model_location_;
+    std::string shape_predict_model_location_;
+    int face_detect_width_ = 640;
+    unsigned long face_aligned_size_ = 112;
+};
+
+}
+
+#endif // FACE_DETECTOR_PARAMS_HPP
