cb22ac423c20baab2f446dcdbe8c3317bcf9a00d
tham <thamngapwei@gmail.com>
2019-02-25 23:32:09 +0800

    add destructor

--- libs/face/key/insight_face_key_extractor_params.hpp
+++ libs/face/key/insight_face_key_extractor_params.hpp
@@ -20,6 +20,7 @@ struct insight_face_key_extractor_params
         model_symbols_(std::move(model_symbols)),
         shape_(shape)
     {}
+    ~insight_face_key_extractor_params(){}
 
     mxnet::cpp::Context context_;
     std::string model_params_;
