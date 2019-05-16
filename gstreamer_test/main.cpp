d2f1696ca6df3535bb94cc1d2acaf265bd3ddcdb
tham <thamngapwei@gmail.com>
2019-05-16 23:24:53 +0800

    perform time management example

--- gstreamer_test/main.cpp
+++ gstreamer_test/main.cpp
@@ -1,11 +1,13 @@
 #include "dynamic_hello_world.hpp"
 #include "gstreamer_concepts.hpp"
 #include "hello_world.hpp"
+#include "time_management.hpp"
 
 int main (int argc, char *argv[])
 {
-    dynamic_hello_world(argc, argv);
+    //dynamic_hello_world(argc, argv);
     //hello_world(argc, argv);
     //gstreamer_concepts(argc,argv);
+    time_management(argc, argv);
 }
 
