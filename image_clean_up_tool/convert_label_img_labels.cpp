48f9e0ac5f4451ee0047e25090cc84ad2bcca25d
tham <thamngapwei@gmail.com>
2020-07-12 21:38:40 +0800

    simplify codes

--- image_clean_up_tool/convert_label_img_labels.cpp
+++ image_clean_up_tool/convert_label_img_labels.cpp
@@ -1,6 +1,8 @@
 #include "convert_label_img_labels.hpp"
 #include "ui_convert_label_img_labels.h"
 
+#include "parser_label_img.hpp"
+
 #include <QDebug>
 #include <QDir>
 #include <QDirIterator>
@@ -13,81 +15,8 @@
 
 namespace{
 
-struct parse_data
-{
-    struct object
-    {
-        QString label_;
-        QRectF rect_;
-    };
-
-    QString abs_file_path_;
-    std::vector<object> objects_;
-
-    void print() const
-    {
-        qDebug()<<__func__<<": file path = "<<abs_file_path_;
-        for(auto const &obj : objects_){
-            qDebug()<<__func__<<": label = "<<obj.label_<<", rect = "<<obj.rect_;
-        }
-    }
-};
-
-QRectF parse_bndbox(QDomElement &elem)
-{
-    QRectF rect;
-    while(!elem.isNull()){
-        if(elem.tagName() == "xmin"){
-            rect.setX(elem.firstChild().toText().data().toDouble());
-        }else if(elem.tagName() == "ymin"){
-            rect.setY(elem.firstChild().toText().data().toDouble());
-        }else if(elem.tagName() == "xmax"){
-            rect.setRight(elem.firstChild().toText().data().toDouble());
-        }else{
-            rect.setBottom(elem.firstChild().toText().data().toDouble());
-        }
-        elem = elem.nextSibling().toElement();
-    }
-
-    return rect;
-}
-
-parse_data parse_xml(QString const &fname)
-{
-    QFile file(QFileInfo(fname).absoluteFilePath());
-    parse_data result;
-    if(file.open(QIODevice::ReadOnly)){
-        QDomDocument doc;
-        doc.setContent(&file);
-        auto dom_elem = doc.firstChild().toElement().firstChild().toElement();
-        while(!dom_elem.isNull()){
-            if(dom_elem.tagName() == "path"){
-                result.abs_file_path_ = dom_elem.firstChild().toText().data();
-            }else if(dom_elem.tagName() == "object"){
-                parse_data::object obj;
-                auto child = dom_elem.firstChild().toElement();
-                while(!child.isNull()){
-                    if(child.tagName() == "name"){
-                        obj.label_ = child.firstChild().toText().data();
-                    }else if(child.tagName() == "bndbox"){
-                        auto bndbox = child.firstChild().toElement();
-                        obj.rect_ = parse_bndbox(bndbox);
-                    }
-
-                    child = child.nextSibling().toElement();
-                }
-                result.objects_.emplace_back(obj);
-            }
-
-            dom_elem = dom_elem.nextSibling().toElement();
-        }
-    }else{
-        qDebug()<<__func__<<"cannot open file = "<<file.fileName();
-    }
-    return result;
-}
-
-void to_alexeyab_yolo_format(std::vector<parse_data> const &input_vec, std::map<QString, int> const &labels_to_int)
+void to_alexeyab_yolo_format(std::vector<parser_label_img::parse_data> const &input_vec,
+                             std::map<QString, int> const &labels_to_int)
 {
     for(auto const &input: input_vec){
         QFileInfo finfo(input.abs_file_path_);
@@ -129,7 +58,7 @@ convert_label_img_labels::~convert_label_img_labels()
 
 void convert_label_img_labels::on_pushButtonAdd_clicked()
 {
-    ui->tableWidget->insertRow(ui->tableWidget->rowCount() > 0 ? ui->tableWidget->rowCount() - 1 : 0);
+    ui->tableWidget->insertRow(ui->tableWidget->rowCount());
 }
 
 void convert_label_img_labels::on_pushButtonRemove_clicked()
@@ -152,11 +81,12 @@ void convert_label_img_labels::on_pushButtonConvert_clicked()
 
     qDebug()<<__func__<<" file paths size = "<<file_paths_.size();
     std::map<QString, int> labels_to_int;
-    std::vector<parse_data> parse_data_vec;
+    std::vector<parser_label_img::parse_data> parse_data_vec;
+    parser_label_img parser;
     int index = 0;
     for(auto const &fname : file_paths_){
         qDebug()<<__func__<<" parse fname = "<<fname;
-        auto parse_result = parse_xml(fname);
+        auto parse_result = parser.parse(fname);
         for(auto const &val : parse_result.objects_){
             if(auto it = labels_to_int.find(val.label_); it == std::end(labels_to_int)){
                 labels_to_int.insert({val.label_, index++});
