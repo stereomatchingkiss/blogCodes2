ed7add67abab47dca607f5235ca95652dfb09e17
tham <thamngapwei@gmail.com>
2020-07-12 14:21:37 +0800

    add parallel algorithms

--- image_clean_up_tool/remove_duplicate_images.cpp
+++ image_clean_up_tool/remove_duplicate_images.cpp
@@ -15,6 +15,8 @@
 #include <QFileInfo>
 #include <QMessageBox>
 
+#include <execution>
+
 using namespace std;
 
 namespace{
@@ -36,9 +38,9 @@ private:
     algo_type algo_;
 };
 
-size_t remove_duplicate(cv::img_hash::ImgHashBase *hash,
-                        std::vector<dist_compare::value_type> const &hash_arr,
-                        double threshold)
+size_t remove_duplicate_seq(cv::img_hash::ImgHashBase *hash,
+                            std::vector<dist_compare::value_type> const &hash_arr,
+                            double threshold)
 {
     size_t removed_file = 0;
     for(size_t i = 0; i < hash_arr.size(); ++i){
@@ -51,7 +53,6 @@ size_t remove_duplicate(cv::img_hash::ImgHashBase *hash,
             for(size_t j = i + 1; j < hash_arr.size(); ++j){
                 if(QFile(std::get<0>(hash_arr[j])).exists()){
                     auto const cmp_value = hash->compare(std::get<1>(hash_arr[i]), std::get<1>(hash_arr[j]));
-                    qDebug()<<__func__<<" threshold = "<<cmp_value<<", "<<threshold;
                     if(cmp_value < threshold){
                         similar_url.emplace_back(std::get<0>(hash_arr[j]), std::get<2>(hash_arr[j]));
                     }
@@ -77,6 +78,47 @@ size_t remove_duplicate(cv::img_hash::ImgHashBase *hash,
     qDebug()<<__func__<<": remove "<<removed_file<<" files";
 
     return removed_file;
+}
+
+size_t remove_duplicate_par(cv::img_hash::ImgHashBase *hash,
+                            std::vector<dist_compare::value_type> const &hash_arr,
+                            double threshold)
+{
+    size_t removed_file = 0;
+    std::mutex mutex_rm;
+    std::for_each(std::execution::par, std::begin(hash_arr), std::end(hash_arr), [&](auto const &val)
+    {
+        auto const &url = std::get<0>(val);
+        if(QFile(url).exists()){
+            std::vector<std::pair<QString, int>> similar_url;
+            for(size_t j = 0; j < hash_arr.size(); ++j){
+                if(QFile(std::get<0>(hash_arr[j])).exists()){
+                    auto const cmp_value = hash->compare(std::get<1>(val), std::get<1>(hash_arr[j]));
+                    if(cmp_value < threshold){
+                        similar_url.emplace_back(std::get<0>(hash_arr[j]), std::get<2>(hash_arr[j]));
+                    }
+                }
+            }
+            if(!similar_url.empty()){
+                similar_url.emplace_back(url, std::get<2>(val));
+                auto max_it = std::max_element(std::begin(similar_url), std::end(similar_url),
+                                               [](auto const &lhs, auto const &rhs)
+                {
+                    return std::get<1>(lhs) < std::get<1>(rhs);
+                });
+                for(auto it = std::begin(similar_url); it != std::end(similar_url); ++it){
+                    if(it != max_it){
+                        std::lock_guard<std::mutex> lock(mutex_rm);
+                        if(QFile(std::get<0>(*it)).exists() && QFile::remove(std::get<0>(*it))){
+                            ++removed_file;
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    return  removed_file;
 }
 
 size_t remove_duplicate(vp_tree<dist_compare::value_type, dist_compare> &tree,
@@ -170,7 +212,7 @@ void remove_duplicate_images::on_pushButtonRemove_clicked()
             tree.create(std::move(hash_arr));
             remove_size = remove_duplicate(tree, image_urls_, ui->spinBoxThreshold->value());
         }else{
-            remove_size = remove_duplicate(phash.get(), hash_arr, ui->spinBoxThreshold->value());
+            remove_size = remove_duplicate_seq(phash.get(), hash_arr, ui->spinBoxThreshold->value());
         }
         ui->labelImageSize->setText(tr("Image size = %1").arg(image_urls_.size() - remove_size));
         QMessageBox::information(this, tr("image_clean_up_tool"), tr("Remove %1 file(s)").arg(remove_size));
