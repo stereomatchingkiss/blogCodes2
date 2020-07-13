23deb94e5ec5d899f806656cfbec33f6b06cc157
tham <thamngapwei@gmail.com>
2020-07-13 15:06:05 +0800

    add new tab to generate image list

--- image_clean_up_tool/mainwindow.cpp
+++ image_clean_up_tool/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 
 #include "convert_label_img_labels.hpp"
+#include "generate_image_list.hpp"
 #include "image_mover.hpp"
 #include "labels_check.hpp"
 #include "remove_duplicate_images.hpp"
@@ -18,6 +19,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->tabWidget->addTab(new split_data, tr("Split data"));
     ui->tabWidget->addTab(new convert_label_img_labels, tr("Convert labels"));
     ui->tabWidget->addTab(new labels_check, tr("Check labels"));
+    ui->tabWidget->addTab(new generate_image_list, tr("Generate image list"));
 }
 
 MainWindow::~MainWindow()
