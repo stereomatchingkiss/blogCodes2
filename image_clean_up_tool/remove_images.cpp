1dafcee7c210289d8395940dc24f0359b1f71539
tham <thamngapwei@gmail.com>
2020-07-15 04:19:36 +0800

    1. Able to remove multiple folders 2. Can remove based on image size

--- image_clean_up_tool/remove_images.cpp
+++ image_clean_up_tool/remove_images.cpp
@@ -2,8 +2,8 @@
 #include "ui_remove_images.h"
 
 #include "utils/iterator_create.hpp"
+#include "utils/utility.hpp"
 
-#include <vp_tree.hpp>
 #include <annoy/src/kissrandom.h>
 #include <annoy/src/annoylib.h>
 
@@ -12,11 +12,13 @@
 #include <opencv2/img_hash.hpp>
 #include <opencv2/core/ocl.hpp>
 
+#include <QCheckBox>
 #include <QDebug>
 #include <QDir>
 #include <QDirIterator>
 #include <QFileDialog>
 #include <QFileInfo>
+#include <QImageReader>
 #include <QMessageBox>
 
 #include <execution>
@@ -29,21 +31,11 @@ struct dist_compare
 {
     using value_type = std::tuple<QString, cv::Mat, int>;
     using algo_type = cv::Ptr<cv::img_hash::ImgHashBase>;
-
-    explicit dist_compare(algo_type algo) :
-        algo_(algo){}
-
-    double operator()(value_type const &lhs, value_type const &rhs) const
-    {
-        return algo_->compare(std::get<1>(lhs), std::get<1>(rhs));
-    }
-
-private:
-    algo_type algo_;
 };
 
 size_t remove_duplicate_seq(cv::img_hash::ImgHashBase *hash,
                             std::vector<dist_compare::value_type> const &hash_arr,
+                            std::vector<dist_compare::value_type> const &hash_arr_can_remove,
                             double threshold)
 {
     size_t removed_file = 0;
@@ -54,25 +46,31 @@ size_t remove_duplicate_seq(cv::img_hash::ImgHashBase *hash,
         }
         if(QFile(url).exists()){
             std::vector<std::pair<QString, int>> similar_url;
-            for(size_t j = i + 1; j < hash_arr.size(); ++j){
-                if(QFile(std::get<0>(hash_arr[j])).exists()){
-                    auto const cmp_value = hash->compare(std::get<1>(hash_arr[i]), std::get<1>(hash_arr[j]));
+            for(size_t j = 0; j < hash_arr_can_remove.size(); ++j){
+                if(QFile(std::get<0>(hash_arr_can_remove[j])).exists() &&
+                        std::get<0>(hash_arr_can_remove[j]) != std::get<0>(hash_arr[i])){
+                    auto const cmp_value = hash->compare(std::get<1>(hash_arr[i]), std::get<1>(hash_arr_can_remove[j]));
                     if(cmp_value < threshold){
-                        similar_url.emplace_back(std::get<0>(hash_arr[j]), std::get<2>(hash_arr[j]));
+                        similar_url.emplace_back(std::get<0>(hash_arr_can_remove[j]),
+                                                 std::get<2>(hash_arr_can_remove[j]));
                     }
                 }
             }
             if(!similar_url.empty()){
-                similar_url.emplace_back(url, std::get<2>(hash_arr[i]));
-                auto max_it = std::max_element(std::begin(similar_url), std::end(similar_url),
-                                               [](auto const &lhs, auto const &rhs)
-                {
-                    return std::get<1>(lhs) < std::get<1>(rhs);
-                });
-                for(auto it = std::begin(similar_url); it != std::end(similar_url); ++it){
-                    if(it != max_it && QFile(std::get<0>(*it)).exists()){
-                        QFile::remove(std::get<0>(*it));
-                        ++removed_file;
+                if(similar_url.size() == 1){
+                    QFile::remove(std::get<0>(similar_url[0]));
+                    ++removed_file;
+                }else{
+                    auto max_it = std::max_element(std::begin(similar_url), std::end(similar_url),
+                                                   [](auto const &lhs, auto const &rhs)
+                    {
+                        return std::get<1>(lhs) < std::get<1>(rhs);
+                    });
+                    for(auto it = std::begin(similar_url); it != std::end(similar_url); ++it){
+                        if(it != max_it && QFile(std::get<0>(*it)).exists()){
+                            QFile::remove(std::get<0>(*it));
+                            ++removed_file;
+                        }
                     }
                 }
             }
@@ -86,6 +84,7 @@ size_t remove_duplicate_seq(cv::img_hash::ImgHashBase *hash,
 
 size_t remove_duplicate_par(cv::img_hash::ImgHashBase *hash,
                             std::vector<dist_compare::value_type> const &hash_arr,
+                            std::vector<dist_compare::value_type> const &hash_arr_can_remove,
                             double threshold)
 {
     size_t removed_file = 0;
@@ -95,26 +94,33 @@ size_t remove_duplicate_par(cv::img_hash::ImgHashBase *hash,
         auto const &url = std::get<0>(val);
         if(QFile(url).exists()){
             std::vector<std::pair<QString, int>> similar_url;
-            for(size_t j = 0; j < hash_arr.size(); ++j){
-                if(QFile(std::get<0>(hash_arr[j])).exists()){
-                    auto const cmp_value = hash->compare(std::get<1>(val), std::get<1>(hash_arr[j]));
+            for(size_t j = 0; j < hash_arr_can_remove.size(); ++j){
+                if(QFile(std::get<0>(hash_arr_can_remove[j])).exists() &&
+                        std::get<0>(hash_arr_can_remove[j]) != url){
+                    auto const cmp_value = hash->compare(std::get<1>(val), std::get<1>(hash_arr_can_remove[j]));
                     if(cmp_value < threshold){
-                        similar_url.emplace_back(std::get<0>(hash_arr[j]), std::get<2>(hash_arr[j]));
+                        similar_url.emplace_back(std::get<0>(hash_arr_can_remove[j]),
+                                                 std::get<2>(hash_arr_can_remove[j]));
                     }
                 }
             }
             if(!similar_url.empty()){
-                similar_url.emplace_back(url, std::get<2>(val));
-                auto max_it = std::max_element(std::begin(similar_url), std::end(similar_url),
-                                               [](auto const &lhs, auto const &rhs)
-                {
-                    return std::get<1>(lhs) < std::get<1>(rhs);
-                });
-                for(auto it = std::begin(similar_url); it != std::end(similar_url); ++it){
-                    if(it != max_it){
-                        std::lock_guard<std::mutex> lock(mutex_rm);
-                        if(QFile(std::get<0>(*it)).exists() && QFile::remove(std::get<0>(*it))){
-                            ++removed_file;
+                if(similar_url.size() == 1){
+                    std::lock_guard<std::mutex> lock(mutex_rm);
+                    QFile::remove(std::get<0>(similar_url[0]));
+                    ++removed_file;
+                }else{
+                    auto max_it = std::max_element(std::begin(similar_url), std::end(similar_url),
+                                                   [](auto const &lhs, auto const &rhs)
+                    {
+                        return std::get<1>(lhs) < std::get<1>(rhs);
+                    });
+                    for(auto it = std::begin(similar_url); it != std::end(similar_url); ++it){
+                        if(it != max_it){
+                            std::lock_guard<std::mutex> lock(mutex_rm);
+                            if(QFile(std::get<0>(*it)).exists() && QFile::remove(std::get<0>(*it))){
+                                ++removed_file;
+                            }
                         }
                     }
                 }
@@ -125,48 +131,7 @@ size_t remove_duplicate_par(cv::img_hash::ImgHashBase *hash,
     return  removed_file;
 }
 
-size_t remove_duplicate(vp_tree<dist_compare::value_type, dist_compare> &tree,
-                        std::vector<QString> const &image_urls,
-                        double threshold)
-{
-    std::vector<dist_compare::value_type> closest_hash;
-    std::vector<double> distance;
-    size_t removed_file = 0;
-    for(size_t i = 0; i < image_urls.size(); ++i){
-        if(i % 100 == 0){
-            qDebug()<<"removed_file:"<<removed_file<<",compare :"<<image_urls[i];
-        }
-        if(QFile(image_urls[i]).exists()){
-            tree.search(tree.get_items()[i], 10, closest_hash, distance,
-                        [=](double val){ return val <= threshold; });
-            if(i % 100 == 0){
-                qDebug()<<"closest_hash:"<<closest_hash.size();
-            }
-            if(closest_hash.size() > 1){
-                std::vector<std::pair<QString, int>> similar_url;
-                for(size_t j = 0; j != closest_hash.size(); ++j){
-                    similar_url.emplace_back(std::get<0>(tree.get_items()[j]), std::get<2>(tree.get_items()[j]));
-                }
-                //keep image with biggest size
-                auto max_it = std::max_element(std::begin(similar_url), std::end(similar_url),
-                                               [](auto const &lhs, auto const &rhs)
-                {
-                    return std::get<1>(lhs) < std::get<1>(rhs);
-                });
-                for(auto it = std::begin(similar_url); it != std::end(similar_url); ++it){
-                    if(it != max_it && QFile(std::get<0>(*it)).exists()){
-                        QFile::remove(std::get<0>(*it));
-                        ++removed_file;
-                    }
-                }
-            }
-        }
-    }
 
-    qDebug()<<__func__<<": remove "<<removed_file<<" files";
-
-    return removed_file;
-}
 
 }
 
@@ -184,19 +149,44 @@ remove_images::~remove_images()
     delete ui;
 }
 
-void remove_images::on_pushButtonSelectFolder_clicked()
+size_t remove_images::remove_img_size_less_than()
 {
-    auto const dir =
-            QFileDialog::getExistingDirectory(this, tr("Select folder to move the file"), ui->lineEditFolder->text());
-    ui->lineEditFolder->setText(dir);
+    size_t remove_size = 0;
+    if(ui->groupBoxRemoveImageLessThanSize->isChecked()){
+        for(size_t i = 0; i != image_urls_.size();){
+            if(image_urls_can_remove_[i]){
+                QImageReader reader(image_urls_[i]);
+                reader.setDecideFormatFromContent(true);
+                if(reader.canRead()){
+                    auto const size = reader.size();
+                    if(size.width() < ui->spinBoxImageWidth->value() ||
+                            size.height() < ui->spinBoxImageHeight->value()){
+                        remove_size += QFile::remove(image_urls_[i]);
+                        image_urls_.erase(image_urls_.begin() + i);
+                        image_urls_can_remove_.erase(image_urls_can_remove_.begin() + i);
+                    }else{
+                        ++i;
+                    }
+                }else{
+                    qDebug()<<__func__<<": cannot read image:"<<image_urls_[i];
+                    ++i;
+                }
+            }else{
+                ++i;
+            }
+        }
+    }
+
+    return remove_size;
 }
 
-void remove_images::on_pushButtonRemove_clicked()
+size_t remove_images::remove_duplicate_images()
 {
-    load_image_urls();
-    if(!image_urls_.empty()){
+    size_t remove_size = 0;
+    if(ui->groupBoxRemoveDuplicateImages->isChecked()){
         auto phash = cv::img_hash::PHash::create();
         std::vector<dist_compare::value_type> hash_arr;
+        std::vector<dist_compare::value_type> hash_arr_can_remove;
         for(size_t i = 0; i != image_urls_.size(); ++i){
             if(i % 100 == 0){
                 qDebug()<<"compute hash of:"<<image_urls_[i];
@@ -206,36 +196,86 @@ void remove_images::on_pushButtonRemove_clicked()
                 cv::Mat hash;
                 phash->compute(img, hash);
                 hash_arr.emplace_back(image_urls_[i], hash, img.cols * img.rows);
+                if(image_urls_can_remove_[i]){
+                    hash_arr_can_remove.emplace_back(image_urls_[i], hash, img.cols * img.rows);
+                }
+            }else{
+                qDebug()<<__func__<<": cannot read image:"<<image_urls_[i];
             }
         }
 
-        size_t remove_size = 0;
-        if(ui->checkBoxFast->isChecked()){
-            dist_compare dc(std::move(phash));
-            vp_tree<dist_compare::value_type, dist_compare> tree(std::move(dc));
-            tree.create(std::move(hash_arr));
-            remove_size = remove_duplicate(tree, image_urls_, ui->spinBoxThreshold->value());
+        if(ui->checkBoxMultiThread->isChecked()){
+            remove_size += remove_duplicate_par(phash.get(), hash_arr,
+                                                hash_arr_can_remove, ui->spinBoxThreshold->value());
         }else{
-            remove_size = remove_duplicate_seq(phash.get(), hash_arr, ui->spinBoxThreshold->value());
+            remove_size += remove_duplicate_seq(phash.get(), hash_arr,
+                                                hash_arr_can_remove, ui->spinBoxThreshold->value());
         }
+    }
+
+    return remove_size;
+}
+
+void remove_images::on_pushButtonRemove_clicked()
+{
+    load_image_urls();
+    if(!image_urls_.empty()){
+        size_t remove_size = remove_img_size_less_than();
+        remove_size += remove_duplicate_images();
+
         ui->labelImageSize->setText(tr("Image size = %1").arg(image_urls_.size() - remove_size));
+
         QMessageBox::information(this, tr("image_clean_up_tool"), tr("Remove %1 file(s)").arg(remove_size));
     }
 }
 
 void remove_images::load_image_urls()
 {
-    if(!QDir(ui->lineEditFolder->text()).exists()){
-        QMessageBox::warning(this, tr("image_clean_up_tool"), tr("Image folder do not exist"));
-        return;
-    }
-
+    image_urls_.clear();
+    image_urls_can_remove_.clear();
     auto const iterate_flag = ui->checkBoxRecursive->isChecked() ?
                 QDirIterator::Subdirectories : QDirIterator::NoIteratorFlags;
-    auto dir_it = create_image_iterator(ui->lineEditFolder->text(), iterate_flag);
-    image_urls_.clear();
-    while(dir_it.hasNext()){
-        image_urls_.emplace_back(QFileInfo(dir_it.next()).absoluteFilePath());
+    for(int i = 0; i != ui->tableWidget->rowCount(); ++i){
+        auto const folder = ui->tableWidget->item(i, 0)->text();
+        auto const should_delete = get_checkbox_state(i, 1);
+        auto dir_it = create_image_iterator(folder, iterate_flag);
+        while(dir_it.hasNext()){
+            auto const url = QFileInfo(dir_it.next()).absoluteFilePath();
+            image_urls_.emplace_back(url);
+            image_urls_can_remove_.emplace_back(should_delete);
+        }
     }
+
     ui->labelImageSize->setText(tr("Image size = %1").arg(image_urls_.size()));
+}
+
+void remove_images::on_pushButtonAddFolder_clicked()
+{
+    auto const row = ui->tableWidget->rowCount();
+    ui->tableWidget->insertRow(row);
+
+    auto *cbox = new QCheckBox;
+    cbox->setChecked(true);
+    ui->tableWidget->setCellWidget(row, 1, place_to_center_widget(cbox));
+}
+
+void remove_images::on_pushButtonFolder_clicked()
+{
+    if(ui->tableWidget->rowCount() > 0){
+        ui->tableWidget->removeRow(ui->tableWidget->rowCount() - 1);
+    }
+}
+
+bool remove_images::get_checkbox_state(int row, int column) const
+{
+    auto *widget = ui->tableWidget->cellWidget(row, column);
+    auto *layout_widget = widget->layout()->itemAt(0)->widget();
+    if(layout_widget){
+        auto *cbox = qobject_cast<QCheckBox*>(layout_widget);
+        if(cbox){
+            return cbox->isChecked();
+        }
+    }
+
+    return false;
 }
--- image_clean_up_tool/remove_images.hpp
+++ image_clean_up_tool/remove_images.hpp
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 
+#include <deque>
 #include <vector>
 
 namespace Ui {
@@ -18,16 +19,22 @@ public:
     ~remove_images();
 
 private slots:
-    void on_pushButtonSelectFolder_clicked();
-
     void on_pushButtonRemove_clicked();
 
+    void on_pushButtonAddFolder_clicked();
+
+    void on_pushButtonFolder_clicked();
+
 private:
+    bool get_checkbox_state(int row, int column) const;
     void load_image_urls();
+    size_t remove_duplicate_images();
+    size_t remove_img_size_less_than();
 
     Ui::remove_images *ui;
 
     std::vector<QString> image_urls_;
+    std::deque<bool> image_urls_can_remove_;
 };
 
 #endif // REMOVE_IMAGES_HPP
