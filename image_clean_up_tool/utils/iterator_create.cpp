53e8474a142dc82e750a3506e2d31ade523ce748
tham <thamngapwei@gmail.com>
2021-02-07 00:29:42 +0800

    support video format

--- image_clean_up_tool/utils/iterator_create.cpp
+++ image_clean_up_tool/utils/iterator_create.cpp
@@ -5,7 +5,8 @@
 
 QDirIterator create_image_iterator(const QString &path, int recursive)
 {
-    return QDirIterator(path, QStringList()<<"*.jpg"<<"*.png"<<"*.bmp"<<"*.tiff"<<"*.jpeg"<<"*.webp"<<"*.gif",
+    return QDirIterator(path, QStringList()<<"*.jpg"<<"*.png"<<"*.bmp"<<"*.tiff"<<"*.jpeg"<<"*.webp"<<"*.gif"
+                        <<"*.avi"<<"*.mp4",
                         QDir::NoDotAndDotDot | QDir::Files | QDir::Dirs,
                         static_cast<QDirIterator::IteratorFlag>(recursive));
 }
