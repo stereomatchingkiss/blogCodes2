df7df8334aad9130eb942074ecceb8c943dae596
stereomatchingkiss <thamngapwei@gmail.com>
2024-01-04 04:41:27 +0800

    make it easier to use

--- image_clean_up_tool/main.cpp
+++ image_clean_up_tool/main.cpp
@@ -5,6 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QCoreApplication::setOrganizationName("ThamSoft");
+    QCoreApplication::setApplicationName("image_clean_up_tool");
+
     MainWindow w;
     w.show();
     return a.exec();
