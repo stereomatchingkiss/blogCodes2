2327e82fc018220d5b70573e0546a6139690d4b4
stereomatchingkiss <thamngapwei@gmail.com>
2024-01-04 04:41:46 +0800

    add new widget

--- image_clean_up_tool/mainwindow.cpp
+++ image_clean_up_tool/mainwindow.cpp
@@ -15,26 +15,28 @@
 #include "show_folders_details.hpp"
 #include "split_data.hpp"
 
+#include "ui/widget_open_image_viewer.hpp"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    str_to_widget_.emplace(tr("crop image"), new image_cropper);
-    str_to_widget_.emplace(tr("move image"), new image_mover);
-    str_to_widget_.emplace(tr("Show folder details"), new show_folders_details);
-    str_to_widget_.emplace(tr("Mask adjustment"), new mask_adjustment_widget);
-    str_to_widget_.emplace(tr("Remove images"), new remove_images);
-    str_to_widget_.emplace(tr("Split data"), new split_data);
-    str_to_widget_.emplace(tr("Convert labels"), new convert_label_img_labels);
-    str_to_widget_.emplace(tr("Check labels"), new labels_check);
-    str_to_widget_.emplace(tr("Generate image list"), new generate_image_list);
-    str_to_widget_.emplace(tr("crop image"), new image_cropper);
-    str_to_widget_.emplace(tr("Convert yolo detect results"), new convert_yolo_detect_results);
     str_to_widget_.emplace(tr("Change wrong image suffix"), new change_wrong_image_suffix);
     str_to_widget_.emplace(tr("Check image state"), new check_images_state);
+    str_to_widget_.emplace(tr("Check labels"), new labels_check);
     str_to_widget_.emplace(tr("Cluster similar images"), new cluster_similar_images);
+    str_to_widget_.emplace(tr("Convert labels"), new convert_label_img_labels);
+    str_to_widget_.emplace(tr("Convert yolo detect results"), new convert_yolo_detect_results);
+    str_to_widget_.emplace(tr("crop image"), new image_cropper);
+    str_to_widget_.emplace(tr("Generate image list"), new generate_image_list);
+    str_to_widget_.emplace(tr("Mask adjustment"), new mask_adjustment_widget);
+    str_to_widget_.emplace(tr("move image"), new image_mover);
+    str_to_widget_.emplace(tr("Remove images"), new remove_images);
+    str_to_widget_.emplace(tr("Show folder details"), new show_folders_details);
+    str_to_widget_.emplace(tr("Split data"), new split_data);
+    str_to_widget_.emplace(tr("Open image viewer"), new widget_open_image_viewer);
 
     for(auto const &val : str_to_widget_){
         ui->stackedWidget->addWidget(val.second);
